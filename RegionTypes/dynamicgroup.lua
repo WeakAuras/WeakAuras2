b40cbd367b177eaa0b43e2dce312c62d89cd0a91
Benjamin Staneck <staneck@gmail.com>
2012-07-06 16:33:56 +0000

    Update TOCs and fix the nasty green texture without adding a hack to LSM like other people

--- RegionTypes/dynamicgroup.lua
+++ RegionTypes/dynamicgroup.lua
@@ -51,8 +51,8 @@ local function modify(parent, region, data)
         region:SetFrameStrata(WeakAuras.frame_strata_types[data.frameStrata]);
     end
     
-    local bgFile = SharedMedia:Fetch("background", data.background or "");
-    local edgeFile = SharedMedia:Fetch("border", data.border or "");
+    local bgFile = data.background ~= 'None' and SharedMedia:Fetch("background", data.background or "");
+    local edgeFile = data.border ~= 'None' and SharedMedia:Fetch("border", data.border or "");
     background:SetBackdrop({
         bgFile = bgFile,
         edgeFile = edgeFile,
