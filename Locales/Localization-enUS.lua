70b492725ab28763a8f35aa2e67d336b9e1be1db
Benjamin Staneck <staneck@gmail.com>
2015-04-10 02:58:43 +0200

    whitespace change to pull updated translations.

--- Locales/Localization-enUS.lua
+++ Locales/Localization-enUS.lua
@@ -416,9 +416,6 @@ L["Zone"] = "Zone"
 
 -- Make missing translations available
 setmetatable(WeakAuras.L, {__index = function(self, key)
-	self[key] = (key or "")
-	
-	
-	
+	self[key] = (key or "")	
 	return key
 end})
