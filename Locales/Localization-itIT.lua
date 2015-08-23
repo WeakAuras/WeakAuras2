c6a4610dddef7e4e07324b7a7b958abd186114a2
Benjamin Staneck <staneck@gmail.com>
2015-08-23 03:58:16 +0200

    add italian locale file

---
+++ Locales/Localization-itIT.lua
@@ -0,0 +1,12 @@
+if not(GetLocale() == "itIT") then
+    return;
+end
+
+local L = WeakAuras.L
+
+-- Options translation
+--@localization(locale="itIT", format="lua_additive_table", namespace="WeakAuras", handle-subnamespaces="none")@
+
+
+--@do-not-package@
+--@end-do-not-package@
