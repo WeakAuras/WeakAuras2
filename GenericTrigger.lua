WeakAuras/WeakAuras2

commit ef5e52e1339280ae454a98625ad9ff06a53ea82f

Move GTFO check code to GenericTrigger.lua
As it belongs there. Some small logic changes, iterate over events
instead of db.displays. Don't check the GTFO version number.
(GTFO 4.2 is now 2 years old.)

GenericTrigger.lua
@@ -140,11 +140,11 @@ function WeakAuras.ScanEvents(event, arg1, arg2, ...)
     for id, triggers in pairs(event_list) do
       WeakAuras.ActivateAuraEnvironment(id);
       for triggernum, data in pairs(triggers) do
-        if(data.trigger) then
-          if(data.trigger(event, arg1, arg2, ...)) then
+        if(data.triggerFunc) then
+          if(data.triggerFunc(event, arg1, arg2, ...)) then
             WeakAuras.ActivateEvent(id, triggernum, data);
           else
-            if(data.untrigger and data.untrigger(event, arg1, arg2, ...)) then
+            if(data.untriggerFunc and data.untriggerFunc(event, arg1, arg2, ...)) then
               WeakAuras.EndEvent(id, triggernum);
             end
           end
@@ -394,8 +394,9 @@ function GenericTrigger.Add(data, region)
 
         events[id] = events[id] or {};
         events[id][triggernum] = {
-          trigger = triggerFunc,
-          untrigger = untriggerFunc,
+          trigger = trigger,
+          triggerFunc = triggerFunc,
+          untriggerFunc = untriggerFunc,
           event = trigger.event,
           events = trigger_events,
           inverse = trigger.use_inverse,
@@ -557,6 +558,19 @@ function GenericTrigger.Modernize(data)
     end
   end
 end
+
+function GenericTrigger.AllAdded()
+  -- Remove GTFO options if GTFO isn't enabled and there are no saved GTFO auras
+  for id, event in pairs(events) do
+    for triggernum, data in pairs(event) do
+      if (data.trigger.event == "GTFO") then
+        return;
+      end
+    end
+  end
+  WeakAuras.event_types["GTFO"] = nil;
+end
+
 
 --#############################
 --# Support code for triggers #

WeakAuras.lua
@@ -651,8 +651,12 @@ loadedFrame:SetScript("OnEvent", function(self, event, addon)
 
       WeakAuras.ResolveCollisions(function() registeredFromAddons = true; end);
       WeakAuras.FixGroupChildrenOrder();
-      WeakAuras.RemoveGTFO();
-
+      
+      for _, triggerSystem in pairs(triggerSystems) do
+        if (triggerSystem.AllAdded) then
+          triggerSystem.AllAdded();
+        end
+      end
       --> check in case of a disconnect during an encounter.
       if (db.CurrentEncounter) then
         WeakAuras.CheckForPreviousEncounter()
@@ -2900,25 +2904,6 @@ function WeakAuras.FixGroupChildrenOrder()
         end
       end
     end
-  end
-end
-
--- Remove GTFO options if GTFO isn't enabled and there are no saved GTFO auras
-function WeakAuras.RemoveGTFO()
-  if not (WeakAuras.loaded_events["GTFO_DISPLAY"] or (GTFO and GTFO.VersionNumber and tonumber(GTFO.VersionNumber) >= 42000)) then
-    for id, data in pairs(db.displays) do
-      if (data.trigger.event == "GTFO") then
-        return;
-      end
-      if (data.numTriggers > 1) then
-        for i, trigger in pairs(data.additional_triggers) do
-          if (trigger.trigger.event == "GTFO") then
-            return;
-          end
-        end
-      end
-    end
-    WeakAuras.event_types["GTFO"] = nil;
   end
 end
 

commit 992f13138e8d9420d64aa8d7662f09a2f54e7d7d

GTFO: Fix logic for hiding GTFO trigger
Broke in ef5e52e1339280ae454a98625ad9ff06a53ea82f. We want to hide
the GTFO trigger if neither a aura uses it nor the addon is loaded.

GenericTrigger.lua
@@ -718,6 +718,9 @@ end
 
 function GenericTrigger.AllAdded()
   -- Remove GTFO options if GTFO isn't enabled and there are no saved GTFO auras
+  if (GTFO) then
+    return
+  end
   for id, event in pairs(events) do
     for triggernum, data in pairs(event) do
       if (data.trigger.event == "GTFO") then