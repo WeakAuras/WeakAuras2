c2e7fce0f58229da610b2448c48b5251904c0977
Benjamin Staneck <staneck@gmail.com>
2016-02-24 20:19:41 +0100

    Bump minor of the widget

--- Options/AceGUI-Widgets/AceGUIWidget-WeakAurasDisplayButton.lua
+++ Options/AceGUI-Widgets/AceGUIWidget-WeakAurasDisplayButton.lua
@@ -1,4 +1,4 @@
-local Type, Version = "WeakAurasDisplayButton", 21
+local Type, Version = "WeakAurasDisplayButton", 22
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
 
