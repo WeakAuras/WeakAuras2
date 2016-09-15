3294297f5f22f0df8bc040c6140c95488bd11c16
Tony Compton <antoinne85@gmail.com>
2016-09-14 20:31:10 -0400

    Removed instancing from the WowApiMock.
    All mocks are now available globally.

--- Utility/Mocks.lua
+++ Utility/Mocks.lua
@@ -1,6 +1,5 @@
 --- Stubs out a mocked method.
 -- Prepares the API Wrapper simulate a scenario you want to test.
--- @param mockInstance The WowApiMock instance that you want to configure.
 --[[-- @param propertyName The name of the property you want to mock. 
 For instance, if you want to mock the method GetRealmName, set this to "RealmName".]]
 --[[-- @param getMethodName The name of the method you want to use for actually calling the function.
@@ -10,7 +9,7 @@ This should be the same name that the WoW API uses.]]
 If you specify a literal (like "Bonechewer") GetRealmName would always return Bonechewer. 
 If you specify a function, then the function will be called whenever someone calls that function on the API. 
 Your function will be passed all of the values that the WoW API would have been passed.]]
-function MockMethodFull(mockInstance, propertyName, getMethodName, setMethodName, defaultValue)
+function MockMethodFull(propertyName, getMethodName, setMethodName, defaultValue)
     table.insert(WowApiMock.BackingProperties.Values, propertyName)
     table.insert(WowApiMock.MockedMethods, getMethodName)
 
@@ -41,32 +40,15 @@ end
 
 --- Stubs out a mocked method.
 -- Prepares the API Wrapper simulate a scenario you want to test.
--- @param mockInstance The WowApiMock instance that you want to configure.
 --[[-- @param methodName The name of the method you want to mock. 
 For instance, if you want to mock the method GetRealmName, set this to "GetRealmName".]]
 --[[-- @param defaultValue The value you want the method to return. 
 If you specify a literal (like "Bonechewer") GetRealmName would always return Bonechewer. 
 If you specify a function, then the function will be called whenever someone calls that function on the API. 
 Your function will be passed all of the values that the WoW API would have been passed.]]
-function MockMethodPartial(mockInstance, methodName, defaultValue)
+function MockMethodPartial(methodName, defaultValue)
     local setMethodName = "Mock"..methodName
     local propertyName = methodName.."BackingProperty"
 
-    MockMethodFull(mockInstance, propertyName, methodName, setMethodName, defaultValue)
-end
-
---- Stubs out a mocked method.
--- Prepares the API Wrapper simulate a scenario you want to test.
--- @param type The WowApiMock instance that you want to configure.
---[[-- @param propertyName The name of the property you want to mock. 
-For instance, if you want to mock the method GetRealmName, set this to "RealmName".]]
---[[-- @param defaultValue The value you want the method to return. 
-If you specify a literal (like "Bonechewer") GetRealmName would always return Bonechewer. 
-If you specify a function, then the function will be called whenever someone calls that function on the API. 
-Your function will be passed all of the values that the WoW API would have been passed.]]
-function MockMethod(type, propertyName, defaultValue)
-    local getMethodName = "Get"..propertyName
-    local setMethodName = "Mock"..getMethodName
-
-    MockMethodFull(type, propertyName, getMethodName, setMethodName, defaultValue)
+    MockMethodFull(propertyName, methodName, setMethodName, defaultValue)
 end
