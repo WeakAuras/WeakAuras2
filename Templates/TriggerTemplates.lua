097f9e99bd8428dc32076b90add68d834eff3477
Benjamin Staneck <staneck@gmail.com>
2016-12-12 23:07:04 +0100

    Revert "Use SetClipsChildren for Texture and Icon Frame"
    This reverts commit 4f8aac7277b4175cb2a14e5e854ba11a5bd5f84e.

--- Templates/TriggerTemplates.lua
+++ Templates/TriggerTemplates.lua
@@ -32,7 +32,6 @@ function WeakAuras.CreateTemplateView(frame)
   newView:SetLayout("fill");
 
   local newViewScroll = AceGUI:Create("ScrollFrame");
-  newViewScroll:SetClipsChildren(true);
   newViewScroll:SetLayout("flow");
   newView:AddChild(newViewScroll);
 
@@ -484,6 +483,7 @@ function WeakAuras.CreateTemplateView(frame)
   end
 
   local newViewBack = CreateFrame("Button", nil, newView.frame, "UIPanelButtonTemplate");
+  newViewBack:SetFrameLevel(100);
   newViewBack:SetScript("OnClick", function()
     if (newView.existingAura) then
       newView.choosenItem = nil;
@@ -499,6 +499,7 @@ function WeakAuras.CreateTemplateView(frame)
   newView.backButton = newViewBack;
 
   local newViewCancel = CreateFrame("Button", nil, newView.frame, "UIPanelButtonTemplate");
+  newViewCancel:SetFrameLevel(100);
   newViewCancel:SetScript("OnClick", function() newView:CancelClose() end);
   newViewCancel:SetPoint("BOTTOMRIGHT", -27, -23);
   newViewCancel:SetHeight(20);
