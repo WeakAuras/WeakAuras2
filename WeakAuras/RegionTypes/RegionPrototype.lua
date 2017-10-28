d6713de0ffdaea71df390d04a7aced59cd7134b3
Infus <infus@squorn.de>
2017-10-28 23:44:44 +0200

    Squelch sounds while in the loading screen
    Try again with a more narrow patch.

--- WeakAuras/RegionTypes/RegionPrototype.lua
+++ WeakAuras/RegionTypes/RegionPrototype.lua
@@ -104,7 +104,7 @@ local function SoundPlayHelper(self)
     return;
   end
 
-  if (WeakAuras.IsOptionsOpen()) then
+  if (WeakAuras.IsOptionsOpen() or WeakAuras.SquelchingActions() or WeakAuras.InLoadingScreen()) then
     return;
   end
 
