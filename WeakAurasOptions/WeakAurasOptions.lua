454a77d656cd64f5c58eff74e6b869842e88373b
Infus <infus@squorn.de>
2017-12-10 15:56:34 +0100

    Remove message box on using a spell id for the aura trigger
    The UnitAura api takes a buff name and not a spell id, thus when
    used with a spell id, we match on any buff that uses the same name.
    
    Due to that we guided people towards using the full scan option.
    
    Usually when people use spell ids, they want it because they want
    their aura to work with all locales and not because there are
    multiple buffs with shared names.
    
    For those, the message box is confussing and probably leads people
    astray.
    
    Github-Issue: 312

--- WeakAurasOptions/WeakAurasOptions.lua
+++ WeakAurasOptions/WeakAurasOptions.lua
@@ -3958,33 +3958,3 @@ function WeakAuras.ShowCloneDialog(data)
     StaticPopup_Show("WEAKAURAS_CLONE_OPTION_ENABLED");
   end
 end
-
-function WeakAuras.ShowSpellIDDialog(trigger, id)
-  if not(odb.preventSpellIDDialog) then
-    StaticPopupDialogs["WEAKAURAS_SPELLID_CHECK"] = {
-      text = L["Spell ID dialog"],
-      button1 = L["Yes"],
-      button2 = L["No"],
-      button3 = L["Never"],
-      OnAccept = function()
-        trigger.fullscan = true;
-        trigger.use_spellId = true;
-        trigger.spellId = id;
-
-        AceConfigDialog:Open("WeakAuras", frame.container);
-      end,
-      OnCancel = function()
-      -- do nothing
-      end,
-      OnAlt = function()
-        odb.preventSpellIDDialog = true
-      end,
-      hideOnEscape = true,
-      whileDead = true,
-      timeout = 0,
-      preferredindex = STATICPOPUP_NUMDIALOGS
-    };
-
-    StaticPopup_Show("WEAKAURAS_SPELLID_CHECK");
-  end
-end
