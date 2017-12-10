767b897ef30832ea0ca50f906a26e54afceb3af0
Infus <infus@squorn.de>
2017-12-10 15:43:48 +0100

    Fix Equipment Set Trigger
    The way the equipment set api works is rather strange. As the ticket
    shows, 9 item sets might use the indexes 0 to 7 and 9, with 8 being
    empty.
    
    Ticket-Nr: 1069

--- WeakAuras/GenericTrigger.lua
+++ WeakAuras/GenericTrigger.lua
@@ -1927,16 +1927,18 @@ function WeakAuras.GetEquipmentSetInfo(itemSetName, partial)
   local bestMatchName = nil;
   local bestMatchIcon = nil;
 
-  for i = 0, C_EquipmentSet.GetNumEquipmentSets() - 1 do
+  for i = 0, C_EquipmentSet.GetNumEquipmentSets() do
     local name, icon, _, _, numItems, numEquipped = C_EquipmentSet.GetEquipmentSetInfo(i);
     if (itemSetName == nil or (name and itemSetName == name)) then
-      local match = (not partial and numItems == numEquipped)
-        or (partial and (numEquipped or 0) > bestMatchNumEquipped);
-      if (match) then
-        bestMatchNumEquipped = numEquipped;
-        bestMatchNumItems = numItems;
-        bestMatchName = name;
-        bestMatchIcon = icon;
+      if (name ~= nil) then
+        local match = (not partial and numItems == numEquipped)
+          or (partial and (numEquipped or 0) > bestMatchNumEquipped);
+        if (match) then
+          bestMatchNumEquipped = numEquipped;
+          bestMatchNumItems = numItems;
+          bestMatchName = name;
+          bestMatchIcon = icon;
+        end
       end
     end
   end
