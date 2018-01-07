7a6d12afe9c54b8211561b1241f0f01365a2427c
Infus <infus@squorn.de>
2018-01-07 19:06:42 +0100

    Icon: Add a "Keep aspect ration" option
    Which crops the icon if the icon is not square
    
    Ticket-Nr: 861

--- WeakAurasOptions/RegionOptions/Icon.lua
+++ WeakAurasOptions/RegionOptions/Icon.lua
@@ -289,11 +289,16 @@ local function createOptions(id, data)
         return not Masque;
       end
     },
+    keepAspectRatio = {
+      type = "toggle",
+      name = "Keep Aspect Ratio",
+      order = 49.1
+    },
     stickyDuration = {
       type = "toggle",
       name = L["Sticky Duration"],
       desc = L["Prevents duration information from decreasing when an aura refreshes. May cause problems if used with multiple auras with different durations."],
-      order = 49.2
+      order = 49.4
     },
     useTooltip = {
       type = "toggle",
