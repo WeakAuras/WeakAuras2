WeakAuras/WeakAuras2

commit 56cba6a261b19f65fbb8c0eb0c082f57ace9bcca

Fix Charges not updating on talent changes
Github-Issue: 458

WeakAuras/Prototypes.lua
@@ -1886,6 +1886,10 @@ WeakAuras.event_prototypes = {
             state.resort = true;
             state.changed = true;
           end
+          if (state.stacks ~= charges) then
+            state.changed = true;
+            state.stacks = charges;
+          end
           state.progressType = 'timed';
         ]=];
       else
@@ -1935,6 +1939,10 @@ WeakAuras.event_prototypes = {
             state.value = nil;
             state.total = nil;
             state.progressType = 'timed';
+          end
+          if (state.stacks ~= charges) then
+            state.changed = true;
+            state.stacks = charges;
           end
         ]=];
         local trackedCharge = tonumber(trigger.trackcharge or 1) or 1;
@@ -2053,9 +2061,6 @@ WeakAuras.event_prototypes = {
         icon = select(3, GetSpellInfo(trigger.spellName or 0));
       end
       return icon;
-    end,
-    stacksFunc = function(trigger)
-      return WeakAuras.GetSpellCharges(trigger.realSpellName);
     end,
     hasSpellID = true,
     automaticrequired = true,

commit 88f749e249a03be7b2e46a4aa4784dd09c796183

Fix %s showing a 1 even if the ability had no charges
This fixes a regression introduced in 56cba6a261b19f65fbb8c0eb0c082f57ace9bcca

The state of CooldownProgress now contains two very related values:

state.stacks is the one behind %s, which magically behaves just like the
user wants %s to behave. That is for abilities that have only one
charge, displays nothing. And doesn't display anything at all for 0
charges.

state.charges instead contains a 1 for ready abilties, even if
technically they aren't even charged abilities. For conditions and the
charges check in the trigger this value is used.

Github-Issue: 492

WeakAuras/Prototypes.lua
@@ -1878,6 +1878,7 @@ WeakAuras.event_prototypes = {
         local showgcd = %s;
         local startTime, duration, gcdCooldown = WeakAuras.GetSpellCooldown(spellname, ignoreRuneCD, showgcd);
         local charges, maxCharges = WeakAuras.GetSpellCharges(spellname);
+        local stacks = maxCharges ~= 1 and charges or nil;
         if (charges == nil) then
           charges = (duration == 0) and 1 or 0;
         end
@@ -1999,15 +2000,16 @@ WeakAuras.event_prototypes = {
         name = "charges",
         display = L["Show if Charges"],
         type = "number",
-      },
-      {
-        name = "stacks",
-        init = "charges",
-        hidden = true,
-        test = "true",
         store = true,
         display = L["Stacks"],
         conditionType = "number"
+      },
+      {
+        name = "stacks",
+        init = "stacks",
+        hidden = true,
+        test = "true",
+        store = true
       },
       {
         hidden  = true,