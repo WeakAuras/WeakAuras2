69549855fde44a2b32697c90ab8917e699974f3a
asakawa-k <alfiegorrell@gmail.com>
2018-07-23 11:00:42 +0100

    Update HelperFunctions.lua
    Increased buffs to check, up to 255 for now.

--- WeakAuras/HelperFunctions.lua
+++ WeakAuras/HelperFunctions.lua
@@ -1,7 +1,7 @@
 local UnitAura = UnitAura
 -- Unit Aura functions that return info about the first Aura matching the spellName or spellID given on the unit.
 local WA_GetUnitAura = function(unit, spell, filter)
-  for i = 1, 40 do
+  for i = 1, 255 do
     local name, _, _, _, _, _, _, _, _, spellId = UnitAura(unit, i, filter)
     if not name then return end
     if spell == spellId or spell == name then
