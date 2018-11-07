08428caee1ba210397931806cf5f0c0681541fee
Benjamin Staneck <staneck@gmail.com>
2018-11-07 16:50:10 +0100

    add a game restart warning
    some people like to update their addon while the client is running, warn them in chat about it

--- WeakAuras/Init.lua
+++ WeakAuras/Init.lua
@@ -1,18 +1,28 @@
 WeakAuras = {}
 WeakAuras.L = {}
 
-local versionString = GetAddOnMetadata("WeakAuras", "Version");
+local versionStringFromToc = GetAddOnMetadata("WeakAuras", "Version");
+local versionString = "@project-version@"
 --@debug@
-if versionString == "@project-version@" then
-  versionString = "Dev"
+if versionStringFromToc == "@project-version@" then
+  versionStringFromToc = "Dev"
 end
 --@end-debug@
-WeakAuras.versionString = versionString
+
+WeakAuras.versionString = versionStringFromToc
 WeakAuras.printPrefix = "|cff9900ffWeakAuras:|r "
 WeakAuras.newFeatureString = "|TInterface\\OptionsFrame\\UI-OptionsFrame-NewFeatureIcon:0|t"
 
 WeakAuras.prettyPrint = function(msg)
   print(WeakAuras.printPrefix .. msg)
+end
+
+WeakAuras.versionMismatchPrint = function()
+  WeakAuras.prettyPrint("You need to restart your game client to complete the WeakAuras update!")
+end
+
+if versionString ~= versionStringFromToc and versionStringFromToc ~= "Dev" then
+  C_Timer.After(1, WeakAuras.versionMismatchPrint)
 end
 
 WeakAuras.PowerAurasPath = "Interface\\Addons\\WeakAuras\\PowerAurasMedia\\Auras\\"
@@ -27,9 +37,12 @@ end
 
 function WeakAuras.StartProfileSystem()
 end
+
 function WeakAuras.StartProfileAura()
 end
+
 function WeakAuras.StopProfileSystem()
 end
+
 function WeakAuras.StopProfileAura()
 end
