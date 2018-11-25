73578d59d88ed2bcb6d2f34ca6adffaa52ad9cf5
Allen Faure <afaure6@gmail.com>
2018-11-24 18:28:16 -0600

    add reset method for collapsed data

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -2055,6 +2055,7 @@ function WeakAuras.Convert(data, newType)
 
   data.regionType = newType;
   WeakAuras.Add(data);
+  WeakAuras.ResetCollapsed(id)
 end
 
 function WeakAuras.DeepCopy(source, dest)
