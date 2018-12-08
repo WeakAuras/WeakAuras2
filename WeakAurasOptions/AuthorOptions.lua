87fe27a3969227f647f6cee8fd7c888b0df76fd6
Allen Faure <afaure6@gmail.com>
2018-12-08 16:35:05 -0600

    fix whitespace

--- WeakAurasOptions/AuthorOptions.lua
+++ WeakAurasOptions/AuthorOptions.lua
@@ -1146,22 +1146,22 @@ local function mergeOptions(childIndex, merged, toMerge)
 end
 
 local function valuesAreEqual(t1, t2)
-	if t1 == t2 then return true end
-	local ty1 = type(t1)
-	local ty2 = type(t2)
-	if ty1 ~= ty2 then return false end
-	if ty1 ~= "table" then return false end
+  if t1 == t2 then return true end
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= "table" then return false end
 
-	for k1, v1 in pairs(t1) do
-		local v2 = t2[k1]
-		if v2 == nil or not valuesAreEqual(v1, v2) then return false end
-	end
- 
-	for k2, v2 in pairs(t2) do
-		local v1 = t1[k2]
-		if v1 == nil or not valuesAreEqual(v1, v2) then return false end
-	end
-	return true
+  for k1, v1 in pairs(t1) do
+    local v2 = t2[k1]
+    if v2 == nil or not valuesAreEqual(v1, v2) then return false end
+  end
+
+  for k2, v2 in pairs(t2) do
+    local v1 = t1[k2]
+    if v1 == nil or not valuesAreEqual(v1, v2) then return false end
+  end
+  return true
 end
 
 local function allChoicesAreDefault(data)
