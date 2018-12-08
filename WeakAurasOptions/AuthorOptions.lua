a78607d377b37e11d0b38133ff3c90289a73dc5c
xMarok <adam@alangbert.com>
2018-12-08 18:12:42 -0500

    Check for tables for default in AuthorOptions (#999)
    Fixes #998

--- WeakAurasOptions/AuthorOptions.lua
+++ WeakAurasOptions/AuthorOptions.lua
@@ -1145,9 +1145,28 @@ local function mergeOptions(childIndex, merged, toMerge)
   end
 end
 
+local function valuesAreEqual(t1, t2)
+  if t1 == t2 then return true end
+  local ty1 = type(t1)
+  local ty2 = type(t2)
+  if ty1 ~= ty2 then return false end
+  if ty1 ~= "table" then return false end
+
+  for k1, v1 in pairs(t1) do
+    local v2 = t2[k1]
+    if v2 == nil or not valuesAreEqual(v1, v2) then return false end
+  end
+
+  for k2, v2 in pairs(t2) do
+    local v1 = t1[k2]
+    if v1 == nil or not valuesAreEqual(v1, v2) then return false end
+  end
+  return true
+end
+
 local function allChoicesAreDefault(data)
   for _, option in ipairs(data.authorOptions) do
-    if option.key ~= nil and option.default ~= nil and option.default ~= data.config[option.key] then
+    if optionClasses[option.type] ~= "noninteractive" and not valuesAreEqual(option.default, data.config[option.key]) then
       return false
     end
   end
