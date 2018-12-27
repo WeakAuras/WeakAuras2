1123efcde56e6894a417a098ad86ff7714ee827e
Infus <infus@squorn.de>
2018-12-27 18:53:15 +0100

    Change fallback icons/names for BuffTrigger 2
    Always fetch the fallback icon on updating a no state match.
    
    This more closely matches what the original BuffTrigger did.
    
    This whole fallback icon/name code is fragile, this fixes a issue where
    after the spells e.g. different specilization, different talents, have
    changed the fallback icon would never update.
    
    It now updates at the next time the aura is not active (or a UNIT_AURA
    event).
    
    Github-Issue: #1059

--- WeakAuras/BuffTrigger2.lua
+++ WeakAuras/BuffTrigger2.lua
@@ -534,13 +534,7 @@ local function UpdateStateWithMatch(time, bestMatch, triggerStates, cloneId, mat
 end
 
 local function UpdateStateWithNoMatch(time, triggerStates, triggerInfo, cloneId, unit, matchCount, unitCount, maxUnitCount, affected, unaffected)
-  if not triggerInfo.fallbackName or not triggerInfo.fallbackName then
-    local fallbackName, fallbackIcon = BuffTrigger.GetNameAndIconSimple(WeakAuras.GetData(triggerInfo.id), triggerInfo.triggernum)
-    if (fallbackName and fallbackIcon) then
-      triggerInfo.fallbackName = fallbackName
-      triggerInfo.fallbackIcon = fallbackIcon
-    end
-  end
+  local fallbackName, fallbackIcon = BuffTrigger.GetNameAndIconSimple(WeakAuras.GetData(triggerInfo.id), triggerInfo.triggernum)
   if not triggerStates[cloneId] then
     triggerStates[cloneId] = {
       show = true,
@@ -559,8 +553,8 @@ local function UpdateStateWithNoMatch(time, triggerStates, triggerInfo, cloneId,
       unit = unit,
       unitName = unit and GetUnitName(unit, false) or "",
       destName = "",
-      name = triggerInfo.fallbackName,
-      icon = triggerInfo.fallbackIcon
+      name = fallbackName,
+      icon = fallbackIcon
     }
     return true
   else
@@ -573,13 +567,13 @@ local function UpdateStateWithNoMatch(time, triggerStates, triggerInfo, cloneId,
       changed = true
     end
 
-    if state.name ~= triggerInfo.fallbackName then
-      state.name = triggerInfo.fallbackName
+    if state.name ~= fallbackName then
+      state.name = fallbackName
       changed = true
     end
 
-    if state.icon ~= triggerInfo.fallbackIcon then
-      state.icon = triggerInfo.fallbackIcon
+    if state.icon ~= fallbackIcon then
+      state.icon = fallbackIcon
       changed = true
     end
 
@@ -1609,14 +1603,6 @@ local function LoadAura(id, triggernum, triggerInfo)
   local filter = triggerInfo.debuffType
   local time = GetTime();
 
-  if not triggerInfo.fallbackName or not triggerInfo.fallbackName then
-    local fallbackName, fallbackIcon = BuffTrigger.GetNameAndIconSimple(WeakAuras.GetData(id), triggernum)
-    if (fallbackName and fallbackIcon) then
-      triggerInfo.fallbackName = fallbackName
-      triggerInfo.fallbackIcon = fallbackIcon
-    end
-  end
-
   if triggerInfo.unit == "multi" then
      AddScanFuncs(triggerInfo, nil, scanFuncNameMulti, scanFuncSpellIdMulti, nil)
   elseif triggerInfo.unit == "group" then
@@ -2082,8 +2068,6 @@ function BuffTrigger.Add(data)
         BuffTrigger.InitMultiAura()
       end
 
-      local fallbackName, fallbackIcon = BuffTrigger.GetNameAndIconSimple(data, triggernum)
-
       local auraspellids
       if trigger.useExactSpellId and trigger.auraspellids then
         auraspellids = {}
@@ -2120,8 +2104,6 @@ function BuffTrigger.Add(data)
         matchCountFunc = matchCountFunc,
         useAffected = trigger.unit == "group" and trigger.useAffected,
         isMulti = trigger.unit == "multi",
-        fallbackName = fallbackName,
-        fallbackIcon = fallbackIcon,
       }
       triggerInfos[id] = triggerInfos[id] or {}
       triggerInfos[id][triggernum] = triggerInformation
