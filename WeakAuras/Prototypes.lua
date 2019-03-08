5c2f3bb40f0231989577016879011ea097da76c7
Nightwarden24 <CodeNightwarden24@gmail.com>
2019-03-08 19:12:32 +0300

    Refactor class and race types; drop the "LibBabble-Race-3.0" library

--- WeakAuras/Prototypes.lua
+++ WeakAuras/Prototypes.lua
@@ -16,8 +16,6 @@ local GetRuneCooldown, UnitCastingInfo, UnitChannelInfo = GetRuneCooldown, UnitC
 local WeakAuras = WeakAuras
 local L = WeakAuras.L
 
--- luacheck: globals C_SpecializationInfo C_Map
-
 local SpellRange = LibStub("SpellRange-1.0")
 function WeakAuras.IsSpellInRange(spellId, unit)
   return SpellRange.IsSpellInRange(spellId, unit)
@@ -586,12 +584,9 @@ WeakAuras.anim_presets = {
 };
 
 WeakAuras.class_ids = {}
-local classID = 1;
-local _, classFileName;
-while(GetClassInfo(classID)) do
-  _, classFileName = GetClassInfo(classID)
-  WeakAuras.class_ids[classFileName] = classID;
-  classID = classID + 1;
+for classID = 1, GetNumClasses() do
+  local classInfo = C_CreatureInfo.GetClassInfo(classID)
+  WeakAuras.class_ids[classInfo.classFile] = classInfo.classID
 end
 
 function WeakAuras.CheckTalentByIndex(index)
