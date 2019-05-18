ad4d79f39c8e7b7264d4ccf58477bf47420362ba
Buds <mrbouyou@gmail.com>
2019-05-18 15:04:20 +0200

    alternate power trigger removed

--- WeakAuras/Prototypes.lua
+++ WeakAuras/Prototypes.lua
@@ -5732,6 +5732,7 @@ WeakAuras.event_prototypes = {
 if WeakAuras.IsClassic then
   WeakAuras.event_prototypes["Threat Situation"] = nil
   WeakAuras.event_prototypes["Death Knight Rune"] = nil
+  WeakAuras.event_prototypes["Alternate Power"] = nil
 end
 
 WeakAuras.dynamic_texts = {
