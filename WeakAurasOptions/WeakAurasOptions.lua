e5108bccfef4b17543e432099679e12d1ff33c21
Grim <bruno.marcetic@gmail.com>
2020-04-01 19:34:00 +0200

    Dont delete a button if its not there

--- WeakAurasOptions/WeakAurasOptions.lua
+++ WeakAurasOptions/WeakAurasOptions.lua
@@ -406,7 +406,7 @@ function WeakAuras.ConstructOptions(prototype, data, startorder, triggernum, tri
       else
         options["use_"..name] = {
           type = "toggle",
-          width = arg.width or WeakAuras.normalWidth,
+          width = WeakAuras.normalWidth,
           name = arg.display,
           order = order,
           hidden = hidden,
@@ -896,7 +896,6 @@ function WeakAuras.ConstructOptions(prototype, data, startorder, triggernum, tri
           name = arg.display,
           order = order,
           values = values,
-          control = arg.control,
           hidden = function()
             return (type(hidden) == "function" and hidden(trigger)) or (type(hidden) ~= "function" and hidden) or trigger["use_"..realname] == false;
           end,
@@ -1295,8 +1294,11 @@ function WeakAuras.DeleteOption(data, massDelete)
 
   frame:ClearPicks();
   WeakAuras.Delete(data);
-  frame.buttonsScroll:DeleteChild(displayButtons[id]);
-  displayButtons[id] = nil;
+  
+  if(displayButtons[id])then
+    frame.buttonsScroll:DeleteChild(displayButtons[id]);
+    displayButtons[id] = nil;
+  end
 
   if(parentData and parentData.controlledChildren and not massDelete) then
     for index, childId in pairs(parentData.controlledChildren) do
@@ -3782,7 +3784,7 @@ function WeakAuras.PositionOptions(id, data, _, hideWidthHeight, disableSelfPoin
       name = L["Anchored To"],
       order = 72,
       hidden = IsParentDynamicGroup,
-      values = (data.regionType == "group" or data.regionType == "dynamicgroup") and WeakAuras.anchor_frame_types_group or WeakAuras.anchor_frame_types,
+      values = WeakAuras.anchor_frame_types,
     },
     -- Input field to select frame to anchor on
     anchorFrameFrame = {
@@ -3821,7 +3823,7 @@ function WeakAuras.PositionOptions(id, data, _, hideWidthHeight, disableSelfPoin
           return L["To Screen's"]
         elseif (data.anchorFrameType == "PRD") then
           return L["To Personal Ressource Display's"];
-        else
+        elseif (data.anchorFrameType == "SELECTFRAME" or data.anchorFrameType == "CUSTOM") then
           return L["To Frame's"];
         end
       end,
@@ -3841,7 +3843,7 @@ function WeakAuras.PositionOptions(id, data, _, hideWidthHeight, disableSelfPoin
     anchorPointGroup = {
       type = "select",
       width = WeakAuras.normalWidth,
-      name = L["To Group's"],
+      name = function() return L["to group's"] end,
       order = 76,
       hidden = function()
         if (data.anchorFrameType ~= "SCREEN") then
