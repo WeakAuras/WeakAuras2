4f21865d8a9830bb6cb46cddf95395bb6e738081
Infus <infus@squorn.de>
2020-07-18 14:03:44 +0200

    Icon: Add missing cooldown = false to defaults
    Unfourtunately that missing cooldown false appears to prevent us from
    changing the default easily.

--- WeakAuras/RegionTypes/Icon.lua
+++ WeakAuras/RegionTypes/Icon.lua
@@ -31,7 +31,7 @@ local default = {
   zoom = 0,
   keepAspectRatio = false,
   frameStrata = 1,
-
+  cooldown = false,
   cooldownTextDisabled = false,
   cooldownSwipe = true,
   cooldownEdge = false,
