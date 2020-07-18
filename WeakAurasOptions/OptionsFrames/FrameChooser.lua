29fb0ff41e8a540ccf0084d1d4e19b5b2ee6ca0b
Infus <infus@squorn.de>
2020-07-18 12:18:10 +0200

    Fix SetBackdrop calls in our code
    Still two errors in AceGuiWidgets-SharedMedia

--- WeakAurasOptions/OptionsFrames/FrameChooser.lua
+++ WeakAurasOptions/OptionsFrames/FrameChooser.lua
@@ -25,6 +25,7 @@ function WeakAuras.StartFrameChooser(data, path)
   if not(frameChooserFrame) then
     frameChooserFrame = CreateFrame("frame");
     frameChooserBox = CreateFrame("frame", nil, frameChooserFrame);
+    Mixin(frameChooserBox, BackdropTemplateMixin)
     frameChooserBox:SetFrameStrata("TOOLTIP");
     frameChooserBox:SetBackdrop({
       edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
