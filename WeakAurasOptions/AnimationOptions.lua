652aa90dbb0523bcf517c58b1ac0b08dddeded8a
Infus <infus@squorn.de>
2020-08-02 19:22:03 +0200

    Remove IsAnimating function
    Every caller passed the wrong arguments, and thus the function always
    returned false. Since that's apparently the case for a long time, just
    remove it.

--- WeakAurasOptions/AnimationOptions.lua
+++ WeakAurasOptions/AnimationOptions.lua
@@ -84,7 +84,7 @@ function WeakAuras.GetAnimationOptions(data)
       data.animation = data.animation or {};
       data.animation[field] = data.animation[field] or {};
       data.animation[field][value] = v;
-      if(field == "main" and not WeakAuras.IsAnimating("display", id)) then
+      if(field == "main") then
         WeakAuras.Animate("display", data.uid, "main", data.animation.main, WeakAuras.regions[id].region, false, nil, true);
         if(WeakAuras.clones[id]) then
           for cloneId, cloneRegion in pairs(WeakAuras.clones[id]) do
@@ -843,12 +843,10 @@ function WeakAuras.GetAnimationOptions(data)
   }
 
   local function extraSetFunction()
-    if(not WeakAuras.IsAnimating("display", id)) then
-      WeakAuras.Animate("display", data.uid, "main", data.animation.main, WeakAuras.regions[id].region, false, nil, true);
-      if(WeakAuras.clones[id]) then
-        for cloneId, cloneRegion in pairs(WeakAuras.clones[id]) do
-          WeakAuras.Animate("display", data.uid, "main", data.animation.main, cloneRegion, false, nil, true, cloneId);
-        end
+    WeakAuras.Animate("display", data.uid, "main", data.animation.main, WeakAuras.regions[id].region, false, nil, true);
+    if(WeakAuras.clones[id]) then
+      for cloneId, cloneRegion in pairs(WeakAuras.clones[id]) do
+        WeakAuras.Animate("display", data.uid, "main", data.animation.main, cloneRegion, false, nil, true, cloneId);
       end
     end
   end
