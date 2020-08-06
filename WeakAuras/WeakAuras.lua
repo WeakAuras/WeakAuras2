953555840f37f0c7241688fdbbc3c31d38a564c0
Infus <infus@squorn.de>
2020-08-06 21:03:09 +0200

    Add a method to get the active state for triggers
    Because I'm nice.

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -3715,6 +3715,9 @@ function WeakAuras.GetTriggerStateForTrigger(id, triggernum)
   return triggerState[id][triggernum];
 end
 
+function WeakAuras.GetActiveTriggers(id)
+  return triggerState[id].triggers
+end
 
 do
   local visibleFakeStates = {}
