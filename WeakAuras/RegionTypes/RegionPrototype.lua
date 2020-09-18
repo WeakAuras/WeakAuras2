WeakAuras/WeakAuras2

commit e010e9d8dbbd5fb4b8149c91993d6a4b5d64c47b

Move FrameTick to local

WeakAuras/RegionTypes/RegionPrototype.lua
@@ -635,6 +635,22 @@ local frameForFrameTick = CreateFrame("FRAME");
 
 WeakAuras.frames["Frame Tick Frame"] = frameForFrameTick
 
+local function FrameTick()
+  if WeakAuras.IsOptionsOpen() then
+    return
+  end
+  Private.StartProfileSystem("frame tick")
+  for region in pairs(regionsForFrameTick) do
+    Private.StartProfileAura(region.id);
+    if region.FrameTick then
+      region.FrameTick()
+    end
+    region.subRegionEvents:Notify("FrameTick")
+    Private.StopProfileAura(region.id);
+  end
+  Private.StopProfileSystem("frame tick")
+end
+
 local function RegisterForFrameTick(region)
   -- Check for a Frame Tick function
   local hasFrameTick = region.FrameTick
@@ -666,21 +682,7 @@ local function UnRegisterForFrameTick(region)
   end
 end
 
-function WeakAuras.FrameTick()
-  if WeakAuras.IsOptionsOpen() then
-    return
-  end
-  Private.StartProfileSystem("frame tick")
-  for region in pairs(regionsForFrameTick) do
-    Private.StartProfileAura(region.id);
-    if region.FrameTick then
-      region.FrameTick()
-    end
-    region.subRegionEvents:Notify("FrameTick")
-    Private.StopProfileAura(region.id);
-  end
-  Private.StopProfileSystem("frame tick")
-end
+
 
 local function TimerTickForSetDuration(self)
   local duration = self.duration

commit 956e7fea6c4944f752d3b8a3895966c0a43f9bdf

Fix FrameTick regression
Introduced in e010e9d8dbbd5 by forgetting to change the one
WeakAuras.FrameTick to FrameTick that matters.

Fixes: #2483

WeakAuras/RegionTypes/RegionPrototype.lua
@@ -671,7 +671,7 @@ local function RegisterForFrameTick(region)
 
   regionsForFrameTick[region] = true
   if not frameForFrameTick:GetScript("OnUpdate") then
-    frameForFrameTick:SetScript("OnUpdate", WeakAuras.FrameTick);
+    frameForFrameTick:SetScript("OnUpdate", FrameTick);
   end
 end
 