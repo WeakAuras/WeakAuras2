285bba8c473ab3e4ec524ac846ee38a1a18dd1b4
Benjamin Staneck <staneck@gmail.com>
2020-10-15 15:54:01 +0200

    Update "Found a Bug?" URL to include the issue template

--- WeakAurasOptions/OptionsFrames/OptionsFrame.lua
+++ WeakAurasOptions/OptionsFrames/OptionsFrame.lua
@@ -481,8 +481,10 @@ function OptionsPrivate.CreateFrame()
 
   addFooter(L["Get Help"], [[Interface\AddOns\WeakAuras\Media\Textures\discord.tga]], "https://discord.gg/wa2",
             L["Chat with WeakAuras experts on our Discord server."])
+
   addFooter(L["Documentation"], [[Interface\AddOns\WeakAuras\Media\Textures\GitHub.tga]], "https://github.com/WeakAuras/WeakAuras2/wiki",
             L["Check out our wiki for a large collection of examples and snippets."])
+
   addFooter(L["Find Auras"], [[Interface\AddOns\WeakAuras\Media\Textures\wagoupdate_logo.tga]], "https://wago.io",
             L["Browse Wago, the largest collection of auras."])
 
@@ -490,7 +492,8 @@ function OptionsPrivate.CreateFrame()
     addFooter(L["Update Auras"], [[Interface\AddOns\WeakAuras\Media\Textures\wagoupdate_refresh.tga]], "https://weakauras.wtf",
             L["Keep your Wago imports up to date with the Companion App."])
   end
-  addFooter(L["Found a Bug?"], [[Interface\AddOns\WeakAuras\Media\Textures\bug_report.tga]], "https://github.com/WeakAuras/WeakAuras2/issues/new",
+
+  addFooter(L["Found a Bug?"], [[Interface\AddOns\WeakAuras\Media\Textures\bug_report.tga]], "https://github.com/WeakAuras/WeakAuras2/issues/new?assignees=&labels=%F0%9F%90%9B+Bug&template=bug_report.md&title=",
             L["Report bugs our our issue tracker."])
 
   -- Disable for now
