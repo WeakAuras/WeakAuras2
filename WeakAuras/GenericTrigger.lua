12499cdb9179281fce2bd95ecaf67fb5e1b6d975
Infus <infus@squorn.de>
2020-11-11 21:32:54 +0100

    Restore a 1px offset to Groups
    But make it a compability option, enabled for existing groups. Disabled
    for new auras.
    
    Fixes: #2696

--- WeakAuras/GenericTrigger.lua
+++ WeakAuras/GenericTrigger.lua
@@ -1371,7 +1371,7 @@ function GenericTrigger.Add(data, region)
           automaticAutoHide = automaticAutoHide,
           tsuConditionVariables = tsuConditionVariables,
           prototype = prototype,
-          ignoreOptionsEventErrors = data.ignoreOptionsEventErrors
+          ignoreOptionsEventErrors = data.information.ignoreOptionsEventErrors
         };
       end
     end
