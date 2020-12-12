843b108e13dd5415504ef3f05ba209d90a542b8b
Infus <infus@squorn.de>
2020-12-12 14:16:18 +0100

    Only call SetBakdrop if we have a edge file
    This can happen if the configured edge file is no longer registered
    with SharedMedia
    
    Fixes: #2809

--- WeakAuras/SubRegionTypes/Border.lua
+++ WeakAuras/SubRegionTypes/Border.lua
@@ -51,13 +51,16 @@ local function modify(parent, region, parentData, data, first)
 
   parent:AnchorSubRegion(region, "area", parentData.regionType == "aurabar" and data.border_anchor, nil, data.border_offset, data.border_offset)
 
-  region:SetBackdrop({
-    edgeFile = SharedMedia:Fetch("border", data.border_edge) or "",
-    edgeSize = data.border_size,
-    bgFile = nil,
-  });
-  region:SetBackdropBorderColor(data.border_color[1], data.border_color[2], data.border_color[3], data.border_color[4])
-  region:SetBackdropColor(0, 0, 0, 0)
+  local edgeFile = SharedMedia:Fetch("border", data.border_edge)
+  if edgeFile and edgeFile ~= "" then
+    region:SetBackdrop({
+      edgeFile = edgeFile,
+      edgeSize = data.border_size,
+      bgFile = nil,
+    })
+    region:SetBackdropBorderColor(data.border_color[1], data.border_color[2], data.border_color[3], data.border_color[4])
+    region:SetBackdropColor(0, 0, 0, 0)
+  end
 
   function region:SetBorderColor(r, g, b, a)
     self:SetBackdropBorderColor(r, g, b, a or 1)
