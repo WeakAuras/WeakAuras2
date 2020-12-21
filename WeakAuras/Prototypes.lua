20c853462e9c6590f642a69e1bd06e07acbfe7dd
nullKomplex <nullKomplex@gmail.com>
2020-12-21 14:24:20 -0700

    Add Item in Range trigger.
    Comes with the following features:
    * Unit and item options
    * Inverse with Target Required option
    * Item name, unit, unit name, and item icon state values

--- WeakAuras/Prototypes.lua
+++ WeakAuras/Prototypes.lua
@@ -8020,6 +8020,105 @@ Private.event_prototypes = {
     hasSpellID = true,
     automaticrequired = true
   },
+
+  ["Item in Range"] = {
+    type = "status",
+    events = {
+      ["events"] = {
+        "PLAYER_TARGET_CHANGED",
+        "WA_SPELL_RANGECHECK",
+      }
+    },
+    name = L["Item in Range"],
+    init = function(trigger)
+      trigger.unit = trigger.unit or "target"
+      trigger.itemName = trigger.itemName or 0
+      local itemName = type(trigger.itemName) == "number" and trigger.itemName or "[["..trigger.itemName.."]]"
+      -- IsItemInRange returns nil for no target, false for out of range, and true for in range.
+      local ret = [=[
+          local unit = %q
+          local item = %q
+          local inverse = %s
+          local targetRequired = %s
+
+          local range = IsItemInRange(item, unit)
+          if range ~= nil then
+            inRange = range == not inverse
+          else
+            inRange = inverse and not targetRequired
+          end
+
+          local name, _, _, _, _, _, _, _, _, icon = GetItemInfo(item);
+          name = name or "Invalid"
+          if not icon then
+            local _, _, _, _, icon = GetItemInfoInstant(item)
+          end
+      ]=]
+      return ret:format(trigger.unit, itemName, trigger.use_inverse and "true" or "false", trigger.use_targetRequired and "true" or "false")
+    end,
+    statesParameter = "one",
+    args = {
+      {
+        name = "unit",
+        display = L["Unit"],
+        required = true,
+        type = "unit",
+        values = "unit_types_range_check",
+        test = "true",
+        store = true
+      },
+      {
+        name = "itemName",
+        display = L["Item"],
+        required = true,
+        type = "item",
+        test = "true",
+      },
+      {
+        name = "inverse",
+        display = L["Inverse"],
+        type = "toggle",
+        test = "true",
+      },
+      {
+        name = "targetRequired",
+        display = L["Require Valid Target"],
+        type = "toggle",
+        enable = function(trigger)
+          return trigger.use_inverse
+        end,
+        test = "true",
+      },
+      {
+        name = "unitName",
+        display = L["Unit Name"],
+        hidden = "true",
+        store = "true",
+        test = "true",
+        init = "GetUnitName(unit)",
+      },
+      {
+        name = "icon",
+        hidden = "true",
+        init = "icon or 'Interface/Icons/INV_Misc_QuestionMark'",
+        store = "true",
+        test = "true",
+      },
+      {
+        name = "name",
+        display = L["Item Name"],
+        hidden = "true",
+        store = "true",
+        test = "true",
+      },
+      {
+        hidden = true,
+        test = "inRange",
+      }
+    },
+    hasItemID = true,
+    automaticrequired = true
+  },
 };
 
 if WeakAuras.IsClassic() then
