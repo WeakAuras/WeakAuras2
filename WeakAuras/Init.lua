a021380042c2a23d814b00541eb8981fa318eeb3
Benjamin Staneck <staneck@gmail.com>
2021-04-18 10:30:09 +0200

    Add TODOs for temp bc hacks

--- WeakAuras/Init.lua
+++ WeakAuras/Init.lua
@@ -27,7 +27,7 @@ local intendedWoWProject = WOW_PROJECT_MAINLINE
 intendedWoWProject = WOW_PROJECT_CLASSIC
 -- @end-version-classic@
 -- @version-bc@
-intendedWoWProject = WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5
+intendedWoWProject = WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5 -- TODO: Remove when every flavor build has the constant
 -- @end-version-bc@
 --@end-non-version-retail@]===]
 
@@ -59,7 +59,7 @@ end
 local intendedWoWProjectName = {
   [WOW_PROJECT_MAINLINE] = "Retail",
   [WOW_PROJECT_CLASSIC] = "Classic",
-  [WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5] = "The Burning Crusade"
+  [WOW_PROJECT_BURNING_CRUSADE_CLASSIC or 5] = "The Burning Crusade" -- TODO: Remove when every flavor build has the constant
 }
 
 Private.wrongTargetMessage = "This version of WeakAuras was packaged for World of Warcraft " .. intendedWoWProjectName[intendedWoWProject] ..
