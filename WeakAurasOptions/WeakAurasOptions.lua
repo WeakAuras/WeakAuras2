df19ed73705bc46b3f0918a7478e24451e255d2e
Infus <infus@squorn.de>
2021-04-25 18:23:42 +0200

    Replace CallbackHandler with a simple callback handler
    CallbackHandler is intended for 1:1 connections, whereas the internal
    callback handler should be a 1:M system.

--- WeakAurasOptions/WeakAurasOptions.lua
+++ WeakAurasOptions/WeakAurasOptions.lua
@@ -539,7 +539,7 @@ function WeakAuras.ToggleOptions(msg, Private)
   end
   if not OptionsPrivate.Private then
     OptionsPrivate.Private = Private
-    OptionsPrivate.Private:RegisterCallback("AuraWarningsUpdated", function(event, uid)
+    OptionsPrivate.Private.callbacks:RegisterCallback("AuraWarningsUpdated", function(event, uid)
       local id = OptionsPrivate.Private.UIDtoID(uid)
       if displayButtons[id] then
         -- The button does not yet exists if a new aura is created
@@ -547,9 +547,9 @@ function WeakAuras.ToggleOptions(msg, Private)
       end
     end)
 
-    OptionsPrivate.Private:RegisterCallback("ScanForLoads", AfterScanForLoads)
-    OptionsPrivate.Private:RegisterCallback("AboutToDelete", OnAboutToDelete)
-    OptionsPrivate.Private:RegisterCallback("Rename", OnRename)
+    OptionsPrivate.Private.callbacks:RegisterCallback("ScanForLoads", AfterScanForLoads)
+    OptionsPrivate.Private.callbacks:RegisterCallback("AboutToDelete", OnAboutToDelete)
+    OptionsPrivate.Private.callbacks:RegisterCallback("Rename", OnRename)
   end
 
   if(frame and frame:IsVisible()) then
