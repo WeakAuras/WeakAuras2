6c87958e7d4a5ebd255a9f621d6f0cc831203e08
Infus <infus@squorn.de>
2021-07-18 18:27:06 +0200

    Sounds: Add Rooster Chicken Calls
    From https://freesound.org/people/AGFX/sounds/43382/
    Sound by AGFX
    
    Licensed under CC0 1.0

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -2379,7 +2379,9 @@ LSM:Register("sound", "Temple Bell", "Interface\\AddOns\\WeakAuras\\Media\\Sound
 LSM:Register("sound", "Torch", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\Torch.ogg")
 LSM:Register("sound", "Warning Siren", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\WarningSiren.ogg")
 LSM:Register("sound", "Lich King Apocalypse", 554003) -- Sound\Creature\LichKing\IC_Lich King_Special01.ogg
+-- Sounds from freesound.org, see commits for attributions
 LSM:Register("sound", "Sheep Blerping", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\SheepBleat.ogg")
+LSM:Register("sound", "Rooster Chicken Call", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\RoosterChickenCalls.ogg")
 
 LSM:Register("sound", "Voice: Adds", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\Adds.ogg")
 LSM:Register("sound", "Voice: Boss", "Interface\\AddOns\\WeakAuras\\Media\\Sounds\\Boss.ogg")
