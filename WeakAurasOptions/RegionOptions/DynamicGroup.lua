f95dde916f720367b0a30da2d4d2b3f061f5609c
Infus <infus@squorn.de>
2022-02-08 23:19:07 +0100

    Fix unlocalized string in Anchor settings

--- WeakAurasOptions/RegionOptions/DynamicGroup.lua
+++ WeakAurasOptions/RegionOptions/DynamicGroup.lua
@@ -134,13 +134,7 @@ local function createOptions(id, data)
       order = 1.5,
       width = WeakAuras.normalWidth,
       name = L["Group by Frame"],
-      desc = L[
-[[Group and anchor each auras by frame.
-
-- Nameplates: attach to nameplates per unit.
-- Unit Frames: attach to unit frame buttons per unit.
-- Custom Frames: choose which frame each region should be anchored to.]]
-      ],
+      desc = L["Group and anchor each auras by frame.\n\n- Nameplates: attach to nameplates per unit.\n- Unit Frames: attach to unit frame buttons per unit.\n- Custom Frames: choose which frame each region should be anchored to."],
       hidden = function() return data.grow == "CUSTOM" end,
     },
     anchorPerUnit = {
