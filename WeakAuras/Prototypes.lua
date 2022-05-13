WeakAuras/WeakAuras2

commit 5de07bf21212392c531b3cdf8404a794def3a354

Optimize load event handling
By storing which auras react to which events

WeakAuras/Prototypes.lua
@@ -870,7 +870,8 @@ WeakAuras.load_prototype = {
       type = "tristate",
       width = WeakAuras.normalWidth,
       init = "arg",
-      optional = true
+      optional = true,
+      events = {"PLAYER_REGEN_DISABLED", "PLAYER_REGEN_ENABLED"}
     },
     {
       name = "encounter",
@@ -878,7 +879,8 @@ WeakAuras.load_prototype = {
       type = "tristate",
       width = WeakAuras.normalWidth,
       init = "arg",
-      optional = true
+      optional = true,
+      events = {"ENCOUNTER_START", "ENCOUNTER_END"}
     },
     {
       name = "warmode",
@@ -888,14 +890,15 @@ WeakAuras.load_prototype = {
       width = WeakAuras.doubleWidth,
       optional = true,
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"UNIT_FLAGS"}
     },
     {
       name = "never",
       display = L["Never"],
       type = "toggle",
       width = WeakAuras.normalWidth,
-      init = "false"
+      init = "false",
     },
     {
       name = "petbattle",
@@ -905,7 +908,8 @@ WeakAuras.load_prototype = {
       width = WeakAuras.normalWidth,
       optional = true,
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PET_BATTLE_OPENING_START", "PET_BATTLE_CLOSE"}
     },
     {
       name = "vehicle",
@@ -915,7 +919,8 @@ WeakAuras.load_prototype = {
       width = WeakAuras.normalWidth,
       optional = true,
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"VEHICLE_UPDATE", "UNIT_ENTERED_VEHICLE", "UNIT_EXITED_VEHICLE", "UPDATE_OVERRIDE_ACTIONBAR"}
     },
     {
       name = "vehicleUi",
@@ -925,7 +930,8 @@ WeakAuras.load_prototype = {
       width = WeakAuras.normalWidth,
       optional = true,
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"VEHICLE_UPDATE", "UNIT_ENTERED_VEHICLE", "UNIT_EXITED_VEHICLE", "UPDATE_OVERRIDE_ACTIONBAR"}
     },
     {
       name = "ingroup",
@@ -934,6 +940,7 @@ WeakAuras.load_prototype = {
       width = WeakAuras.normalWidth,
       init = "arg",
       values = "group_types",
+      events = {"GROUP_LEFT", "GROUP_JOINED"}
     },
     {
       name = "name",
@@ -1003,7 +1010,8 @@ WeakAuras.load_prototype = {
       end,
       init = "arg",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_TALENT_UPDATE"}
     },
     {
       name = "class_and_spec",
@@ -1012,7 +1020,8 @@ WeakAuras.load_prototype = {
       values = "spec_types_all",
       init = "arg",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_TALENT_UPDATE"}
     },
     {
       name = "talent",
@@ -1020,6 +1029,7 @@ WeakAuras.load_prototype = {
       type = "multiselect",
       values = valuesForTalentFunction,
       test = "WeakAuras.CheckTalentByIndex(%d)",
+      events = {"PLAYER_TALENT_UPDATE"}
     },
     {
       name = "talent2",
@@ -1030,6 +1040,7 @@ WeakAuras.load_prototype = {
       enable = function(trigger)
         return trigger.use_talent ~= nil or trigger.use_talent2 ~= nil;
       end,
+      events = {"PLAYER_TALENT_UPDATE"}
     },
     {
       name = "talent3",
@@ -1040,6 +1051,7 @@ WeakAuras.load_prototype = {
       enable = function(trigger)
         return (trigger.use_talent ~= nil and trigger.use_talent2 ~= nil) or trigger.use_talent3 ~= nil;
       end,
+      events = {"PLAYER_TALENT_UPDATE"}
     },
     {
       name = "pvptalent",
@@ -1101,13 +1113,15 @@ WeakAuras.load_prototype = {
       end,
       test = "WeakAuras.CheckPvpTalentByIndex(%d)",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_PVP_TALENT_UPDATE"}
     },
     {
       name = "spellknown",
       display = L["Spell Known"],
       type = "spell",
       test = "WeakAuras.IsSpellKnown(%s)",
+      events = {"SPELLS_CHANGED"}
     },
     {
       name = "race",
@@ -1128,6 +1142,7 @@ WeakAuras.load_prototype = {
       display = L["Player Level"],
       type = "number",
       init = "arg",
+      events = {"PLAYER_LEVEL_UP"}
     },
     {
       name = "effectiveLevel",
@@ -1137,12 +1152,14 @@ WeakAuras.load_prototype = {
       desc = L["The effective level differs from the level in e.g. Time Walking dungeons."],
       enable = not WeakAuras.IsClassic(),
       hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_LEVEL_UP", "UNIT_FLAGS", "ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "zone",
       display = L["Zone Name"],
       type = "string",
       init = "arg",
+      events = {"ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "zoneId",
@@ -1153,6 +1170,7 @@ WeakAuras.load_prototype = {
       test = "WeakAuras.CheckNumericIds(%q, zoneId)",
       enable = not WeakAuras.IsClassic(),
       hidden = WeakAuras.IsClassic(),
+      events = {"ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "zonegroupId",
@@ -1163,6 +1181,7 @@ WeakAuras.load_prototype = {
       test = "WeakAuras.CheckNumericIds(%q, zonegroupId)",
       enable = not WeakAuras.IsClassic(),
       hidden = WeakAuras.IsClassic(),
+      events = {"ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "encounterid",
@@ -1173,6 +1192,7 @@ WeakAuras.load_prototype = {
       test = "WeakAuras.CheckNumericIds(%q, encounterid)",
       enable = not WeakAuras.IsClassic(),
       hidden = WeakAuras.IsClassic(),
+      events = {"ENCOUNTER_START", "ENCOUNTER_END"}
     },
     {
       name = "size",
@@ -1181,6 +1201,7 @@ WeakAuras.load_prototype = {
       values = "instance_types",
       init = "arg",
       control = "WeakAurasSortedDropdown",
+      events = {"ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "difficulty",
@@ -1189,7 +1210,8 @@ WeakAuras.load_prototype = {
       values = "difficulty_types",
       init = "arg",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_DIFFICULTY_CHANGED", "ZONE_CHANGED", "ZONE_CHANGED_INDOORS", "ZONE_CHANGED_NEW_AREA"}
     },
     {
       name = "role",
@@ -1198,7 +1220,8 @@ WeakAuras.load_prototype = {
       values = "role_types",
       init = "arg",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"PLAYER_ROLES_ASSIGNED"}
     },
     {
       name = "affixes",
@@ -1208,7 +1231,8 @@ WeakAuras.load_prototype = {
       init = "arg",
       test = "WeakAuras.CheckMPlusAffixIds(%d, affixes)",
       enable = not WeakAuras.IsClassic(),
-      hidden = WeakAuras.IsClassic()
+      hidden = WeakAuras.IsClassic(),
+      events = {"CHALLENGE_MODE_START", "CHALLENGE_MODE_COMPLETED"}
     },
   }
 };

WeakAuras/WeakAuras.lua
@@ -173,6 +173,8 @@ local in_loading_screen = false;
 local loadFuncs = {};
 -- Load functions for the Options window that ignore various load options
 local loadFuncsForOptions = {};
+-- Mapping of events to ids, contains true if a aura should be checked for a certain event
+local loadEvents = {}
 
 -- Check Conditions Functions, keyed on id
 local checkConditions = {};
@@ -537,6 +539,7 @@ function WeakAuras.ConstructFunction(prototype, trigger, skipOptional)
   local required = {};
   local tests = {};
   local debug = {};
+  local events = {}
   local init;
   if(prototype.init) then
     init = prototype.init(trigger);
@@ -639,6 +642,13 @@ function WeakAuras.ConstructFunction(prototype, trigger, skipOptional)
           else
             tinsert(tests, test);
           end
+
+          if test and arg.events then
+            for index, event in ipairs(arg.events) do
+              events[event] = true
+            end
+          end
+
           if(arg.debug) then
             tinsert(debug, arg.debug:format(trigger[name]));
           end
@@ -659,7 +669,7 @@ function WeakAuras.ConstructFunction(prototype, trigger, skipOptional)
   end
   ret = ret.."return true else return false end end";
 
-  return ret;
+  return ret, events;
 end
 
 function WeakAuras.GetActiveConditions(id, cloneId)
@@ -1852,10 +1862,13 @@ end
 
 local toLoad = {}
 local toUnload = {};
-local function scanForLoadsImpl(self, event, arg1, ...)
+local function scanForLoadsImpl(toCheck, event, arg1, ...)
   if (WeakAuras.IsOptionsProcessingPaused()) then
     return;
   end
+
+  toCheck = toCheck or loadEvents[event or "SCAN_ALL"]
+
   -- PET_BATTLE_CLOSE fires twice at the end of a pet battle. IsInBattle evaluates to TRUE during the
   -- first firing, and FALSE during the second. I am not sure if this check is necessary, but the
   -- following IF statement limits the impact of the PET_BATTLE_CLOSE event to the second one.
@@ -1876,6 +1889,10 @@ local function scanForLoadsImpl(self, event, arg1, ...)
   elseif (event == "ENCOUNTER_END") then
     encounter_id = 0
     WeakAuras.DestroyEncounterTable()
+  end
+
+  if toCheck == nil or next(toCheck) == nil then
+    return
   end
 
   local player, realm, spec, zone = UnitName("player"), GetRealmName(), WeakAuras.IsClassic() and 1 or GetSpecialization(), GetRealZoneText();
@@ -1934,7 +1951,6 @@ local function scanForLoadsImpl(self, event, arg1, ...)
     group = "solo";
   end
 
-
   local affixes, warmodeActive, effectiveLevel = 0, false, 0
   if not WeakAuras.IsClassic() then
     effectiveLevel = UnitEffectiveLevel("player")
@@ -1946,7 +1962,9 @@ local function scanForLoadsImpl(self, event, arg1, ...)
   local shouldBeLoaded, couldBeLoaded;
   wipe(toLoad);
   wipe(toUnload);
-  for id, data in pairs(db.displays) do
+
+  for id in pairs(toCheck) do
+    local data = WeakAuras.GetData(id)
     if (data and not data.controlledChildren) then
       local loadFunc = loadFuncs[id];
       local loadOpt = loadFuncsForOptions[id];
@@ -2008,11 +2026,11 @@ local function scanForLoadsImpl(self, event, arg1, ...)
   wipe(toUnload)
 end
 
-function WeakAuras.ScanForLoads(self, event, arg1, ...)
+function WeakAuras.ScanForLoads(toCheck, event, arg1, ...)
   if not WeakAuras.IsLoginFinished() then
     return
   end
-  scanForLoadsImpl(self, event, arg1, ...)
+  scanForLoadsImpl(toCheck, event, arg1, ...)
 end
 
 local loadFrame = CreateFrame("FRAME");
@@ -2055,16 +2073,16 @@ if not WeakAuras.IsClassic() then
 end
 
 function WeakAuras.RegisterLoadEvents()
-  loadFrame:SetScript("OnEvent", function(...)
+  loadFrame:SetScript("OnEvent", function(frame, ...)
     WeakAuras.StartProfileSystem("load");
-    WeakAuras.ScanForLoads(...)
+    WeakAuras.ScanForLoads(nil, ...)
     WeakAuras.StopProfileSystem("load");
   end);
 
-  unitLoadFrame:SetScript("OnEvent", function(s, e, arg1, ...)
+  unitLoadFrame:SetScript("OnEvent", function(frame, e, arg1, ...)
     WeakAuras.StartProfileSystem("load");
     if (arg1 == "player") then
-      WeakAuras.ScanForLoads(...)
+      WeakAuras.ScanForLoads(nil, e, arg1, ...)
     end
     WeakAuras.StopProfileSystem("load");
   end);
@@ -2236,6 +2254,9 @@ function WeakAuras.Delete(data)
   loaded[id] = nil;
   loadFuncs[id] = nil;
   loadFuncsForOptions[id] = nil;
+  for event, eventData in pairs(loadEvents) do
+    eventData[id] = nil
+  end
   checkConditions[id] = nil;
   conditionChecksTimers.recheckTime[id] = nil;
   if (conditionChecksTimers.recheckHandle[id]) then
@@ -2307,6 +2328,11 @@ function WeakAuras.Rename(data, newid)
 
   loadFuncsForOptions[newid] = loadFuncsForOptions[oldid]
   loadFuncsForOptions[oldid] = nil;
+
+  for event, eventData in pairs(loadEvents) do
+    eventData[newid] = eventData[oldid]
+    eventData[oldid] = nil
+  end
 
   checkConditions[newid] = checkConditions[oldid];
   checkConditions[oldid] = nil;
@@ -3859,12 +3885,21 @@ local function pAdd(data)
       data.triggers.activeTriggerMode = WeakAuras.trigger_modes.first_active;
     end
 
-
     for _, triggerSystem in pairs(triggerSystems) do
       triggerSystem.Add(data);
     end
 
-    local loadFuncStr = WeakAuras.ConstructFunction(load_prototype, data.load);
+    local loadFuncStr, events = WeakAuras.ConstructFunction(load_prototype, data.load);
+    for event, eventData in pairs(loadEvents) do
+      eventData[id] = nil
+    end
+    for event in pairs(events) do
+      loadEvents[event] = loadEvents[event] or {}
+      loadEvents[event][id] = true
+    end
+    loadEvents["SCAN_ALL"] = loadEvents["SCAN_ALL"] or {}
+    loadEvents["SCAN_ALL"][id] = true
+
     local loadForOptionsFuncStr = WeakAuras.ConstructFunction(load_prototype, data.load, true);
     local loadFunc = WeakAuras.LoadFunction(loadFuncStr);
     local loadForOptionsFunc = WeakAuras.LoadFunction(loadForOptionsFuncStr);
@@ -3918,7 +3953,7 @@ local function pAdd(data)
 
     if not(paused) then
       region:Collapse();
-      WeakAuras.ScanForLoads();
+      WeakAuras.ScanForLoads({[id] = true});
     end
   end
 

commit 6676270fc6df26f36d71be42f918eb9ab6112bae

Fix Warmode load condition
Probably a regression from 5de07bf21

Fixes: #3619

WeakAuras/Prototypes.lua
@@ -1283,7 +1283,7 @@ Private.load_prototype = {
       optional = true,
       enable = WeakAuras.IsRetail(),
       hidden = not WeakAuras.IsRetail(),
-      events = {"UNIT_FLAGS"}
+      events = {"PLAYER_FLAGS_CHANGED"}
     },
     {
       name = "never",

WeakAuras/WeakAuras.lua
@@ -1616,6 +1616,7 @@ unitLoadFrame:RegisterUnitEvent("UNIT_FLAGS", "player");
 if WeakAuras.IsRetail() then
   unitLoadFrame:RegisterUnitEvent("UNIT_ENTERED_VEHICLE", "player");
   unitLoadFrame:RegisterUnitEvent("UNIT_EXITED_VEHICLE", "player");
+  unitLoadFrame:RegisterUnitEvent("PLAYER_FLAGS_CHANGED", "player");
 end
 
 function Private.RegisterLoadEvents()