37fa3de875af35290e3b62aeee7dedf58ca91cdf
Infus <infus@squorn.de>
2022-09-08 00:25:22 +0200

    Fix Slider max >= min check
    By doing the same check that AceGui does and fixing it up if we detect a
    problem.
    
    Noteably that means soft overrides normal, and fallbacks to 0 and 100
    respectively.
    
    Fixes: #3799

--- WeakAurasOptions/AuthorOptions.lua
+++ WeakAurasOptions/AuthorOptions.lua
@@ -676,8 +676,18 @@ typeControlAdders = {
     bigStep = option.bigStep
     min = option.min
     max = option.max
-    if max and min then
-      max = math.max(min, max)
+    local effectiveMin = softMin or min or 0
+    local effectiveMax = softMax or max or 100
+    if (effectiveMin > effectiveMax) then
+      -- This will cause a error inside the slider
+      -- Fix up either softMax or max, depending on which one is the effective one
+      if softMax then
+        softMax = effectiveMin
+      elseif max then
+        max = effectiveMin
+      else
+        softMax = effectiveMin
+      end
     end
     step = option.step
     args[prefix .. "default"] = {
@@ -2223,13 +2233,23 @@ local function addUserModeOption(options, args, data, order, prefix, i)
       userOption.get = getUserNumAsString(option)
       userOption.set = setUserNum(data, option)
     elseif optionType == "range" then
-      userOption.softMax = option.softMax
+      userOption.softMax = option.softMax or 20
       userOption.softMin = option.softMin
       userOption.bigStep = option.bigStep
       userOption.min = option.min
       userOption.max = option.max
-      if userOption.max and userOption.min then
-        userOption.max = max(userOption.min, userOption.max)
+      local effectiveMin = userOption.softMin or userOption.min or 0
+      local effectiveMax = userOption.softMax or userOption.max or 100
+      if (effectiveMin > effectiveMax) then
+        -- This will cause a error inside the slider
+        -- Fix up either softMax or max, depending on which one is the effective one
+        if userOption.softMax then
+          userOption.softMax = effectiveMin
+        elseif userOption.max then
+          userOption.max = effectiveMin
+        else
+          userOption.softMax = effectiveMin
+        end
       end
       userOption.step = option.step
     elseif optionType == "color" then
