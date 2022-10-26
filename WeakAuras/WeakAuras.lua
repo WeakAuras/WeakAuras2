7f7d8b95235606775363da6431d8fe10f70f4263
Infus <infus@squorn.de>
2022-10-26 19:36:23 +0200

    Prevent lua error in WeakAuras.PreAdd if passed nil
    Fixes: #2801

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -2797,6 +2797,7 @@ function Private.UpdateSoundIcon(data)
 end
 
 function WeakAuras.PreAdd(data)
+  if not data then return end
   -- Readd what Compress removed before version 8
   if (not data.internalVersion or data.internalVersion < 7) then
     Private.validate(data, oldDataStub)
