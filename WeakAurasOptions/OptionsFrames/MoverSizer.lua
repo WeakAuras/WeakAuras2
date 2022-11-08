fee9f912108e2313810442964b7274e455c73b6e
Infus <infus@squorn.de>
2022-11-08 14:11:57 +0100

    MoverSizer: Skip regions that are anchoring restricted
    Since we can't get their position. Fixes the lua error reported in
     #3996, but has nothing to do with the rest of the issue.

--- WeakAurasOptions/OptionsFrames/MoverSizer.lua
+++ WeakAurasOptions/OptionsFrames/MoverSizer.lua
@@ -383,7 +383,7 @@ local function BuildAlignLines(mover)
 
   for k, v in pairs(OptionsPrivate.displayButtons) do
     local region = WeakAuras.GetRegion(v.data.id)
-    if not skipIds[k] and v.view.visibility ~= 0 and region then
+    if not skipIds[k] and v.view.visibility ~= 0 and region and not region:IsAnchoringRestricted() then
       local scale = region:GetEffectiveScale() / UIParent:GetEffectiveScale()
       if not IsControlKeyDown() then
         tinsert(x, (region:GetLeft() or 0) * scale)
