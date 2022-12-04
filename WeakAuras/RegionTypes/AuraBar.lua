585473f4e878d9b78a8fc112c60113fa9aee4c44
Buds <mrbouyou@gmail.com>
2022-12-04 23:36:55 +0100

    AuraBar gradient: fix bug on classic
    Fixes #4099

--- WeakAuras/RegionTypes/AuraBar.lua
+++ WeakAuras/RegionTypes/AuraBar.lua
@@ -559,7 +559,7 @@ local barPrototype = {
       self.fg:SetGradient(orientation, CreateColor(r1, g1, b1, a1),
                                        CreateColor(r2, g2, b2, a2))
     else
-      self.fg:SetGradientAlpha(self.direction, r1, g1, b1, a1, r2, g2, b2, a2)
+      self.fg:SetGradientAlpha(orientation, r1, g1, b1, a1, r2, g2, b2, a2)
     end
   end,
 
