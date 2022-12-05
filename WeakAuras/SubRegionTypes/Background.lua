d820d4360e100258e08357f8aad26f43154bf22d
Infus <infus@squorn.de>
2022-12-05 19:57:57 +0100

    Background Sub Element: Fix formatting

--- WeakAuras/SubRegionTypes/Background.lua
+++ WeakAuras/SubRegionTypes/Background.lua
@@ -4,56 +4,56 @@ local AddonName, Private = ...
 local L = WeakAuras.L;
 
 do
-    local function subSupports(regionType)
-        return regionType ~= "group" and regionType ~= "dynamicgroup"
-    end
+  local function subSupports(regionType)
+    return regionType ~= "group" and regionType ~= "dynamicgroup"
+  end
 
-    local function noop()
-    end
+  local function noop()
+  end
 
-    local function subSetFrameLevel(self, level)
-        self.parent:SetFrameLevel(level)
-    end
+  local function subSetFrameLevel(self, level)
+    self.parent:SetFrameLevel(level)
+  end
 
-    local function subCreate()
-        return { Update = noop, SetFrameLevel = subSetFrameLevel}
-    end
+  local function subCreate()
+    return { Update = noop, SetFrameLevel = subSetFrameLevel}
+  end
 
-    local function subModify(parent, region)
-        region.parent = parent
-    end
+  local function subModify(parent, region)
+    region.parent = parent
+  end
 
-    WeakAuras.RegisterSubRegionType("subbackground", L["Background"], subSupports, subCreate, subModify,
-                                    noop, noop, {}, nil, {}, false)
+  WeakAuras.RegisterSubRegionType("subbackground", L["Background"], subSupports, subCreate, subModify,
+                                  noop, noop, {}, nil, {}, false)
 end
 
 -- Foreground for aurabar
 
 do
-    local function subSupports(regionType)
-        return regionType == "aurabar"
-    end
+  local function subSupports(regionType)
+    return regionType == "aurabar"
+  end
 
-    local function noop()
-    end
+  local function noop()
+  end
 
-    local function subSetFrameLevel(self, level)
-        if self.parent.bar then
-            self.parent.bar:SetFrameLevel(level)
-        end
-        if self.parent.iconFrame then
-            self.parent.iconFrame:SetFrameLevel(level)
-        end
+  local function subSetFrameLevel(self, level)
+    if self.parent.bar then
+      self.parent.bar:SetFrameLevel(level)
     end
-
-    local function subCreate()
-        return { Update = noop, SetFrameLevel = subSetFrameLevel}
+    if self.parent.iconFrame then
+      self.parent.iconFrame:SetFrameLevel(level)
     end
+  end
 
-    local function subModify(parent, region)
-        region.parent = parent
-    end
+  local function subCreate()
+    return { Update = noop, SetFrameLevel = subSetFrameLevel}
+  end
 
-    WeakAuras.RegisterSubRegionType("subforeground", L["Foreground"], subSupports, subCreate, subModify,
-                                    noop, noop, {}, nil, {}, false)
+  local function subModify(parent, region)
+    region.parent = parent
+  end
+
+  WeakAuras.RegisterSubRegionType("subforeground", L["Foreground"], subSupports, subCreate, subModify,
+                                  noop, noop, {}, nil, {}, false)
 end
