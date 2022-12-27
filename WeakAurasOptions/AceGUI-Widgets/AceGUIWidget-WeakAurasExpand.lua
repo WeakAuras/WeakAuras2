41d5be7c06fcbe4dd4bb7bce72a2dd5dccb3a634
Allen Faure <afaure6@gmail.com>
2022-12-27 13:03:24 -0600

    bump widget version
    Signed-off-by: Allen Faure <afaure6@gmail.com>

--- WeakAurasOptions/AceGUI-Widgets/AceGUIWidget-WeakAurasExpand.lua
+++ WeakAurasOptions/AceGUI-Widgets/AceGUIWidget-WeakAurasExpand.lua
@@ -3,7 +3,7 @@ Button Widget for our Expand button
 -------------------------------------------------------------------------------]]
 if not WeakAuras.IsLibsOK() then return end
 
-local Type, Version = "WeakAurasExpand", 4
+local Type, Version = "WeakAurasExpand", 5
 
 local AceGUI = LibStub and LibStub("AceGUI-3.0", true)
 if not AceGUI or (AceGUI:GetWidgetVersion(Type) or 0) >= Version then return end
