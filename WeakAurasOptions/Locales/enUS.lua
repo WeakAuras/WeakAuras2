aa48a2eeb598ed3564fe9460db621f882787d598
Håkon Solbjørg <hakon@solbj.org>
2023-01-23 22:45:48 +0100

    chore(typo): Fix typo for example in enUS locale

--- WeakAurasOptions/Locales/enUS.lua
+++ WeakAurasOptions/Locales/enUS.lua
@@ -212,7 +212,7 @@ If the entered number is a decimal (e.g. 0.5), fraction (e.g. 1/2), or percentag
 
 |cFF4444FFFor example:|r
 |cFF00CC00> 0|r will trigger when any unit of type '%s' is affected
-|cFF00CC00= 100%%|r will trigger when ever unit of type '%s' is affected
+|cFF00CC00= 100%%|r will trigger when every unit of type '%s' is affected
 |cFF00CC00!= 2|r will trigger when the number of units of type '%s' affected is not exactly 2
 |cFF00CC00<= 0.8|r will trigger when less than 80%% of the units of type '%s' is affected (4 of 5 party members, 8 of 10 or 20 of 25 raid members)
 |cFF00CC00> 1/2|r will trigger when more than half of the units of type '%s' is affected
