53a6653a5c687e202838b44b664adcc6fe4bff00
Buds <mrbouyou@gmail.com>
2023-04-22 19:36:07 +0200

    Text sub element: add condition for editing text

--- WeakAuras/Conditions.lua
+++ WeakAuras/Conditions.lua
@@ -57,6 +57,11 @@ local function formatValueForAssignment(vType, value, pathToCustomFunction, path
       return tostring(value)
     end
     return "nil"
+  elseif (vType == "string") then
+    if type(value) == "string" then
+      return string.format("%s", Private.QuotedString(value))
+    end
+    return "nil"
   elseif(vType == "color") then
     if (value and type(value) == "table") then
       return string.format("{%s, %s, %s, %s}",
@@ -120,7 +125,7 @@ local function formatValueForAssignment(vType, value, pathToCustomFunction, path
 end
 
 local function formatValueForCall(type, property)
-  if (type == "bool" or type == "number" or type == "list" or type == "icon") then
+  if (type == "bool" or type == "number" or type == "list" or type == "icon" or type == "string") then
     return "propertyChanges['" .. property .. "']";
   elseif (type == "color") then
     local pcp = "propertyChanges['" .. property .. "']";
