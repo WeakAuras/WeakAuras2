ce10315325ef723321395cdf89cdd45dc9e554b7
Infus <infus@squorn.de>
2023-05-01 16:08:22 +0200

    SubscribaleObject: Prevent double subscription
    By tracking the subscribers per event

--- WeakAuras/SubscribableObject.lua
+++ WeakAuras/SubscribableObject.lua
@@ -6,7 +6,8 @@ local WeakAuras = WeakAuras
 local L = WeakAuras.L
 
 --- @class SubscribableObject
---- @field events table<string, frame[]>
+--- @field events table<string, frame[]> Subscribers ordered by "priority"
+--- @field subscribers table<string, frame> Subscribers lookup
 --- @field callback table<string, fun():nil>
 --- @field ClearSubscribers fun(self: SubscribableObject)
 --- @field ClearCallbacks fun(self: SubscribableObject)
@@ -21,6 +22,7 @@ local SubscribableObject =
   --- @type fun(self: SubscribableObject)
   ClearSubscribers = function(self)
     self.events = {}
+    self.subscribers = {}
   end,
 
   --- @type fun(self: SubscribableObject)
@@ -36,6 +38,12 @@ local SubscribableObject =
     end
 
     self.events[event] = self.events[event] or {}
+    self.subscribers[event] = self.subscribers[event] or {}
+    if self.subscribers[event][subscriber] then
+      -- Already subscribed, just return
+      return
+    end
+    self.subscribers[event][subscriber] = true
     local pos = highPriority and 1 or (#self.events[event] + 1)
     if TableHasAnyEntries(self.events[event]) then
       tinsert(self.events[event], pos, subscriber)
@@ -47,9 +55,17 @@ local SubscribableObject =
     end
   end,
 
+
+
   --- @type fun(self: SubscribableObject, event: string, subscriber: frame)
   RemoveSubscriber = function(self, event, subscriber)
     if self.events[event] then
+      if not self.subscribers[event][subscriber] then
+        -- Not subscribed
+        return
+      end
+
+      self.subscribers[event][subscriber] = nil
       local index = tIndexOf(self.events[event], subscriber)
       if index then
         tremove(self.events[event], index)
@@ -87,6 +103,7 @@ function Private.CreateSubscribableObject()
   for f, func in pairs(SubscribableObject) do
     system[f] = func
     system.events = {}
+    system.subscribers = {}
     system.callbacks = {}
   end
   return system
