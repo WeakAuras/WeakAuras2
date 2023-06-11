88d8043cf0a63a4826aa77f0c809d11d09ef69b9
Infus <infus@squorn.de>
2023-06-11 14:00:48 +0200

    Animation Options: Fix rotate amount being invisible
    Fixes: #4489

--- WeakAurasOptions/AnimationOptions.lua
+++ WeakAurasOptions/AnimationOptions.lua
@@ -630,7 +630,7 @@ function OptionsPrivate.GetAnimationOptions(data)
         softMax = 360,
         bigStep = 3,
         hidden = function()
-          return (data.animation.main.type ~= "custom" or not OptionsPrivate.Private.EnsureRegion(id).Rotate)
+          return (data.animation.main.type ~= "custom" or not OptionsPrivate.Private.EnsureRegion(id).SetAnimRotation)
         end
       },
       main_use_color = {
