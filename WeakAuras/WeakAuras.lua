234abc9253e4be85e57152fc5c32bb90bb1a030f
Buds <mrbouyou@gmail.com>
2023-07-08 12:38:36 +0200

    Action - Chat message: fix sending to a unit on an other server #4517

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -3333,7 +3333,7 @@ function Private.HandleChatAction(message_type, message, message_dest, message_d
         message_dest = Private.ReplacePlaceHolders(message_dest, region, customFunc, useHiddenStates, formatters);
       end
       if message_dest_isunit == true then
-        message_dest = UnitName(message_dest)
+        message_dest = GetUnitName(message_dest, true)
       end
       pcall(function() SendChatMessage(message, "WHISPER", nil, message_dest) end);
     end
