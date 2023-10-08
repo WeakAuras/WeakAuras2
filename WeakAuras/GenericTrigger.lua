WeakAuras/WeakAuras2

commit ca76ed1175bd386a9cdcb0a223643659bf53c059

Fix PlayerMoving trigger
In certain conditions thus as with dragon riding, we no events even
though IsPlayerMoving changes. Thus check in an every frame handler.

This will only be active for those that somehow have a IsMoving trigger,
thus it really shouldn't affect anyone.

Fixes: #4393

WeakAuras/GenericTrigger.lua
@@ -4008,19 +4008,12 @@ do
   local playerMovingFrame = nil
   local moving;
 
-  local function PlayerMoveUpdate(self, event)
+  local function PlayerMoveUpdate()
     Private.StartProfileSystem("generictrigger");
-    -- channeling e.g. Mind Flay results in lots of PLAYER_STARTED_MOVING, PLAYER_STOPPED_MOVING
-    -- for each frame
-    -- So check after 0.01 s if IsPlayerMoving() actually returns something different.
-    timer:ScheduleTimer(function()
-      Private.StartProfileSystem("generictrigger");
-      if (moving ~= IsPlayerMoving() or moving == nil) then
-        moving = IsPlayerMoving();
-        WeakAuras.ScanEvents("PLAYER_MOVING_UPDATE")
-      end
-      Private.StopProfileSystem("generictrigger");
-    end, 0.01);
+    if (moving ~= IsPlayerMoving() or moving == nil) then
+      moving = IsPlayerMoving();
+      WeakAuras.ScanEvents("PLAYER_MOVING_UPDATE")
+    end
     Private.StopProfileSystem("generictrigger");
   end
 
@@ -4039,9 +4032,7 @@ do
       playerMovingFrame = CreateFrame("Frame");
       Private.frames["Player Moving Frame"] =  playerMovingFrame;
     end
-    playerMovingFrame:RegisterEvent("PLAYER_STARTED_MOVING");
-    playerMovingFrame:RegisterEvent("PLAYER_STOPPED_MOVING");
-    playerMovingFrame:SetScript("OnEvent", PlayerMoveUpdate)
+    playerMovingFrame:SetScript("OnUpdate", PlayerMoveUpdate)
   end
 
   function WeakAuras.WatchPlayerMoveSpeed()

commit 4826c6f81ee04f78851c27a4e64b70b978751ed4

Fix regression in "Continously update Move Speed"
Introduced in ca76ed1175bd386a9cdcb0, which changed the IsPlayerMoving
check to not listen to events but check every frame. That every frame
handler overwrote the MoveSpeed every frame handler.

So combine the handlers since they both want to run every frame.

This has been broken for 6m.

Fixes: #4630

WeakAuras/GenericTrigger.lua
@@ -3633,22 +3633,23 @@ end
 
 -- Player Moving
 do
+  --- @class PlayerMovingFrame
+  --- @field moving integer|nil
+  --- @field speed integer|nil
+
+  --- @type PlayerMovingFrame|Frame|nil
   local playerMovingFrame = nil
-  local moving;
 
   local function PlayerMoveUpdate()
     Private.StartProfileSystem("generictrigger");
-    if (moving ~= IsPlayerMoving() or moving == nil) then
-      moving = IsPlayerMoving();
+    local moving = IsPlayerMoving()
+    if (playerMovingFrame.moving ~= moving or playerMovingFrame.moving == nil) then
+      playerMovingFrame.moving = moving
       WeakAuras.ScanEvents("PLAYER_MOVING_UPDATE")
     end
-    Private.StopProfileSystem("generictrigger");
-  end
 
-  local function PlayerMoveSpeedUpdate()
-    Private.StartProfileSystem("generictrigger");
     local speed = GetUnitSpeed("player")
-    if speed ~= playerMovingFrame.speed then
+    if playerMovingFrame.speed ~= speed then
       playerMovingFrame.speed = speed
       WeakAuras.ScanEvents("PLAYER_MOVE_SPEED_UPDATE")
     end
@@ -3658,18 +3659,11 @@ do
   function WeakAuras.WatchForPlayerMoving()
     if not(playerMovingFrame) then
       playerMovingFrame = CreateFrame("Frame");
+      --- @cast playerMovingFrame PlayerMovingFrame
       Private.frames["Player Moving Frame"] =  playerMovingFrame;
+      playerMovingFrame.speed = GetUnitSpeed("player")
     end
     playerMovingFrame:SetScript("OnUpdate", PlayerMoveUpdate)
-  end
-
-  function WeakAuras.WatchPlayerMoveSpeed()
-    if not(playerMovingFrame) then
-      playerMovingFrame = CreateFrame("Frame");
-      Private.frames["Player Moving Frame"] =  playerMovingFrame;
-    end
-    playerMovingFrame.speed = GetUnitSpeed("player")
-    playerMovingFrame:SetScript("OnUpdate", PlayerMoveSpeedUpdate)
   end
 end
 

WeakAuras/Prototypes.lua
@@ -8867,7 +8867,7 @@ Private.event_prototypes = {
       }
     },
     internal_events = function(trigger, untrigger)
-      local events = { "WA_DELAYED_PLAYER_ENTERING_WORLD", "PLAYER_MOVING_UPDATE" }
+      local events = { "WA_DELAYED_PLAYER_ENTERING_WORLD" }
       if trigger.use_moveSpeed then
         tinsert(events, "PLAYER_MOVE_SPEED_UPDATE")
       end
@@ -8875,9 +8875,8 @@ Private.event_prototypes = {
     end,
     loadFunc = function(trigger)
       if trigger.use_moveSpeed then
-        WeakAuras.WatchPlayerMoveSpeed()
+        WeakAuras.WatchForPlayerMoving()
       end
-      WeakAuras.WatchForPlayerMoving()
     end,
     init = function()
       local ret = [[