1d56b3a883186438bfb204c48ec2b9cf84e866ed
Infus <infus@squorn.de>
2023-12-05 14:13:49 +0100

    Options: Ignore newFeatureString for sorting

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -5969,6 +5969,15 @@ function Private.SortOrderForValues(values)
   table.sort(sortOrder, function(aKey, bKey)
     local aValue = values[aKey]
     local bValue = values[bKey]
+
+    if aValue:sub(1, #WeakAuras.newFeatureString) == WeakAuras.newFeatureString then
+      aValue = aValue:sub(#WeakAuras.newFeatureString + 1)
+    end
+
+    if bValue:sub(1, #WeakAuras.newFeatureString) == WeakAuras.newFeatureString then
+      bValue = bValue:sub(#WeakAuras.newFeatureString + 1)
+    end
+
     return aValue < bValue
   end)
   return sortOrder
