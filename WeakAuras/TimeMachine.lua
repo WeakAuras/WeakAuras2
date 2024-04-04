047350b41abff450beea2e859fbd0e1d2ebf229f
Allen Faure <afaure6@gmail.com>
2024-04-04 16:17:26 -0500

    improve time machine annotation
    Signed-off-by: Allen Faure <afaure6@gmail.com>

--- WeakAuras/TimeMachine.lua
+++ WeakAuras/TimeMachine.lua
@@ -1,5 +1,7 @@
----@type string, Private
-local _, Private = ...
+---@type string
+local AddonName = ...
+---@class Private
+local Private = select(2, ...)
 
 ---@class TimeMachine
 local TimeMachine = {
