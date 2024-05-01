51e7e18884fa0267f0ae20630e27c7875d36b990
Infus <infus@squorn.de>
2024-05-01 22:20:23 +0200

    Options: Add a Thanks button
    Which includes a curated list of thanks + an automatically updated
    patreon list.

---
+++ WeakAuras/PatreonList.lua
@@ -0,0 +1,14 @@
+if not WeakAuras.IsLibsOK() then return end
+---@type string
+local AddonName = ...
+---@class Private
+local Private = select(2, ...)
+Private.DiscordList = {
+
+}
+Private.DiscordListCJ = {
+
+}
+Private.DiscordListK = {
+
+}
