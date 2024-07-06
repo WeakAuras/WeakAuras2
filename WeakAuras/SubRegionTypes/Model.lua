9914e1d07bd69803b296eba73f41a6955cc2cf23
Infus <infus@squorn.de>
2024-07-06 21:28:33 +0200

    TWW+Model SubElement: Replace "Clips by Progress" by "Anchor to FG"
    In TWW, the SetClipsChildren api no longer clips PlayerModels, thus the
    old implementation for making the model be on the foreground no longer
    works.
    
    Replace that option with the similar directly attaching of the model to
    the foreground. This does look worse but after tinkering for a good bit,
    that seems to be the only possibility.

--- WeakAuras/SubRegionTypes/Model.lua
+++ WeakAuras/SubRegionTypes/Model.lua
@@ -94,7 +94,11 @@ local function AcquireModel(region, data)
 
   local anchor
   if region.parentType == "aurabar" then
-    anchor = region.parent.bar
+    if data.bar_model_clip and WeakAuras.IsTWW() then
+      anchor = region.parent.bar.fgMask
+    else
+      anchor = region.parent.bar
+    end
   else
     anchor = region.parent
   end
@@ -190,7 +194,9 @@ local funcs = {
 
 local function create()
   local subRegion = CreateFrame("Frame", nil, UIParent)
-  subRegion:SetClipsChildren(true)
+  if not WeakAuras.IsTWW() then
+    subRegion:SetClipsChildren(true)
+  end
 
   for k, v in pairs(funcs) do
     subRegion[k] = v
@@ -207,8 +213,6 @@ end
 local function onRelease(subRegion)
   subRegion:Hide()
 end
-
-
 
 local function modify(parent, region, parentData, data, first)
   if region.model then
