5ed8700b337d00d0e0ec782e93b97d217a280f7c
Buds <mrbouyou@gmail.com>
2024-07-09 00:54:06 +0200

    documentation and wiki workflow for create create WeakAurasAPI.lua with lua-language-server

---
+++ .github/scripts/docs/update-documentation-file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+lua-language-server --doc="." --doc_out_path=".github/scripts/docs" --logpath=".github/scripts/docs" --metapath=".github/scripts/docs"
+
+# jq '[.[] | select(.name | index("WeakAuras") != -1)]' doc.json > output.json
+
+node .github/scripts/docs/parser.js > WeakAurasOptions/WeakAurasAPI.lua
