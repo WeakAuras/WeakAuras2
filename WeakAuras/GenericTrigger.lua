WeakAuras/WeakAuras2

commit 60c5a604b008c8250372c762a27a251622eaa2bb

Temporary Enchants: Use WEAPON_ENCHANGT_CHANGED on retail
Should hopefully fix the linked bug report

Fixes: #5508

WeakAuras/GenericTrigger.lua
@@ -4023,8 +4023,12 @@ do
   function WeakAuras.TenchInit()
     if not(tenchFrame) then
       tenchFrame = CreateFrame("Frame");
-      tenchFrame:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player");
       tenchFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
+      if WeakAuras.IsRetail() then
+        tenchFrame:RegisterEvent("WEAPON_ENCHANT_CHANGED")
+      else
+        tenchFrame:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
+      end
       if WeakAuras.IsClassicEra() then
         tenchFrame:RegisterEvent("PLAYER_EQUIPMENT_CHANGED");
       end
@@ -4120,7 +4124,7 @@ do
 
       tenchFrame:SetScript("OnEvent", function()
         Private.StartProfileSystem("generictrigger temporary enchant");
-        timer:ScheduleTimer(tenchUpdate, 0.1);
+        timer:ScheduleTimer(tenchUpdate, 0.1)
         Private.StopProfileSystem("generictrigger temporary enchant");
       end);
 

commit 604f693d4d50ef6fa0331f3821de88d6db0d98dc

Enchant Trigger: Fix regression on weapon switching
Introduced in 60c5a604b0

Fixes: #5566

WeakAuras/GenericTrigger.lua
@@ -4063,9 +4063,8 @@ do
       tenchFrame:RegisterEvent("PLAYER_ENTERING_WORLD");
       if WeakAuras.IsRetail() then
         tenchFrame:RegisterEvent("WEAPON_ENCHANT_CHANGED")
-      else
-        tenchFrame:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
       end
+      tenchFrame:RegisterUnitEvent("UNIT_INVENTORY_CHANGED", "player")
       if WeakAuras.IsClassicEra() then
         tenchFrame:RegisterEvent("PLAYER_EQUIPMENT_CHANGED");
       end