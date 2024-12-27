c6531aa09b1b7d4f0d7c3634f255417403fb0da2
InfusOnWoW <infus@squorn.de>
2024-12-27 11:56:19 +0100

    Fix regressions in Textures refactor
    Fixes: #5597

--- WeakAuras/RegionTypes/Texture.lua
+++ WeakAuras/RegionTypes/Texture.lua
@@ -154,6 +154,10 @@ local function modify(parent, region, data)
     self:UpdateProgress()
   end
 
+  function region:SetTexture(texture)
+    self.texture:SetTexture(texture)
+  end
+
   region.texture:SetTexture(data.texture)
 
   function region:Color(r, g, b, a)
