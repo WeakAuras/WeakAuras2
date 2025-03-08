ce650419b172ccb705ed9e1e807ea742955cb3eb
Infus <infus@squorn.de>
2025-03-09 00:46:19 +0100

    StopMotion: Fix up/down buttons for start/end percent
    Fixes: #5732

--- WeakAurasOptions/RegionOptions/StopMotion.lua
+++ WeakAurasOptions/RegionOptions/StopMotion.lua
@@ -228,7 +228,7 @@ local function createOptions(id, data)
             name = L["Animation Start"],
             min = 0,
             max = 1,
-            --bigStep = 0.01,
+            bigStep = 0.01,
             order = 13,
             isPercent = true
         },
@@ -239,7 +239,7 @@ local function createOptions(id, data)
             name = L["Animation End"],
             min = 0,
             max = 1,
-            --bigStep  = 0.01,
+            bigStep  = 0.01,
             order = 14,
             isPercent = true
         },
