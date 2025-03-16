09a14d508eb4f2bf36b21b3f043bd77a8dc89455
Benjamin Staneck <staneck@gmail.com>
2025-03-16 19:20:50 +0100

    pull enUS locale for Options from CF

--- WeakAurasOptions/Locales/enUS.lua
+++ WeakAurasOptions/Locales/enUS.lua
@@ -1,3 +1,9 @@
 if not WeakAuras.IsLibsOK() then return end
 
+if ((GAME_LOCALE or GetLocale()) ~= "enUS") or ((GAME_LOCALE or GetLocale()) ~= "enGB") then
+  return
+end
+
 local L = WeakAuras.L
+
+--@localization(locale="enUS", format="lua_additive_table", namespace="WeakAuras / Options")@
