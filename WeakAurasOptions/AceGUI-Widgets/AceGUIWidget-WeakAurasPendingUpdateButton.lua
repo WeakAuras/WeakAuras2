f2baee1d55483eeeac9fa8608555b98fda4f98fb
Buds <mrbouyou@gmail.com>
2025-03-24 18:35:03 +0100

    UpdatePendingButton: add context menu with list of linked auras
    fixes #5731

--- WeakAurasOptions/AceGUI-Widgets/AceGUIWidget-WeakAurasPendingUpdateButton.lua
+++ WeakAurasOptions/AceGUI-Widgets/AceGUIWidget-WeakAurasPendingUpdateButton.lua
@@ -70,8 +70,29 @@ local methods = {
       WeakAuras.Import(self.companionData.encoded, nil, nil, linkedAuras)
     end
 
+    function self.callbacks.OnFollowLinkClick()
+      self.menu = {
+        { text = L["Linked Auras"], isTitle = true }
+      }
+      for auraId in pairs(self.linkedAuras) do
+        if not self.linkedChildren[auraId] then
+          tinsert(
+            self.menu,
+            {
+              text = auraId,
+              notCheckable = true,
+              func = function() WeakAuras.PickDisplay(auraId, "information") end
+            }
+          )
+        end
+      end
+      LibDD:EasyMenu(self.menu, WeakAuras_DropDownMenu, self.followLink, 0, 0, "MENU", 5)
+    end
+
     self:SetTitle(self.companionData.name)
+    self.frame:SetScript("OnClick", self.callbacks.OnClickNormal)
     self.update:SetScript("OnClick", self.callbacks.OnUpdateClick)
+    self.followLink:SetScript("OnClick", self.callbacks.OnFollowLinkClick)
     local data = OptionsPrivate.Private.StringToTable(self.companionData.encoded, true)
     WeakAuras.PreAdd(data.d)
     self.data = data.d
@@ -162,9 +183,6 @@ local methods = {
   ["SetTitle"] = function(self, title)
     self.titletext = title
     self.title:SetText(title)
-  end,
-  ["SetClick"] = function(self, func)
-    self.frame:SetScript("OnClick", func)
   end,
   ["ResetLinkedAuras"] = function(self)
     wipe(self.linkedAuras)
@@ -289,6 +307,22 @@ local function Constructor()
 
   button.description = {}
 
+  -- follow link button
+  local followLink = CreateFrame("Button", nil, button)
+  button.followLink = followLink
+  followLink:SetNormalAtlas("loottoast-arrow-green", true)
+  followLink:GetNormalTexture():SetRotation(math.rad(-90))
+  followLink:SetSize(24, 24)
+  followLink:SetPoint("RIGHT", button, "RIGHT", -2, 0)
+  followLink:SetScript("OnEnter", function()
+    GameTooltip:SetOwner(followLink, "ANCHOR_NONE")
+    GameTooltip:SetPoint("BOTTOMLEFT", followLink, "TOPRIGHT")
+    GameTooltip:ClearLines()
+    GameTooltip:AddLine(L["Linked Auras"])
+    GameTooltip:Show()
+  end)
+  followLink:SetScript("OnLeave", Hide_Tooltip)
+
   local update = CreateFrame("Button", nil, button)
   button.update = update
   update.disabled = true
@@ -298,7 +332,7 @@ local function Constructor()
   update:Disable()
   update:SetWidth(24)
   update:SetHeight(24)
-  update:SetPoint("RIGHT", button, "RIGHT", -2, 0)
+  update:SetPoint("RIGHT", followLink, "LEFT", -2, 0)
 
   -- Add logo
   local updateLogo = CreateFrame("Frame", nil, button)
@@ -327,7 +361,13 @@ local function Constructor()
   end)
   update:SetScript("OnEnter", function()
     animGroup:Play()
+    GameTooltip:SetOwner(update, "ANCHOR_NONE")
+    GameTooltip:SetPoint("BOTTOMLEFT", update, "TOPRIGHT")
+    GameTooltip:ClearLines()
+    GameTooltip:AddLine(L["Update"])
+    GameTooltip:Show()
   end)
+  update:SetScript("OnLeave", Hide_Tooltip)
   update:Hide()
   updateLogo:Hide()
 
@@ -339,6 +379,7 @@ local function Constructor()
     background = background,
     update = update,
     updateLogo = updateLogo,
+    followLink = followLink,
     type = Type,
   }
 
