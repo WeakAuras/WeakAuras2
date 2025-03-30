WeakAuras/WeakAuras2

commit 86bc2d91bbd5a569015084f2e7ebddfcad0bb7ce

Sub Regions: Fix progress not updating on trigger deactivation
And tweak display of infinite progress.

Fixes: #5707

WeakAuras/RegionTypes/RegionPrototype.lua
@@ -408,7 +408,22 @@ local function UpdateProgressFromState(self, minMaxConfig, state, progressSource
   local remainingProperty = progressSource[8]
   local useAdditionalProgress = progressSource[9]
 
-  if progressType == "number" then
+  if not state or not state.show then
+    self.minProgress, self.maxProgress = nil, nil
+    self.progressType = "timed"
+    self.duration = 0
+    self.expirationTime = math.huge
+    self.modRate = nil
+    self.inverse = false
+    self.paused = true
+    self.remaining = math.huge
+    if self.UpdateTime then
+      self:UpdateTime()
+    end
+    if self.SetAdditionalProgress then
+      self:SetAdditionalProgress(nil)
+    end
+  elseif progressType == "number" then
     local value = state[property]
     if type(value) ~= "number" then value = 0 end
     local total = totalProperty and state[totalProperty]

WeakAuras/SubRegionTypes/CircularProgressTexture.lua
@@ -207,9 +207,15 @@ local funcs = {
       elseif progressData.progressType == "timed" then
         if progressData.paused then
           local remaining = self.progressData.remaining
-          local progress = remaining / self.progressData.duration
-          if self.inverse then
-            progress = 1 - progress
+          local duration = self.progressData.duration
+          local progress
+          if duration == 0 then
+            progress = 0
+          else
+            progress = remaining / self.progressData.duration
+            if self.inverse then
+              progress = 1 - progress
+            end
           end
           self.circularTexture:SetProgress(self:ProgressToAngles(progress))
         else

WeakAuras/SubRegionTypes/LinearProgressTexture.lua
@@ -172,9 +172,15 @@ local funcs = {
       elseif progressData.progressType == "timed" then
         if progressData.paused then
           local remaining = self.progressData.remaining
-          local progress = remaining / self.progressData.duration
-          if self.inverse then
-            progress = 1 - progress
+          local duration = self.progressData.duration
+          local progress
+          if duration == 0 then
+            progress = 0
+          else
+            progress = remaining / duration
+            if self.inverse then
+              progress = 1 - progress
+            end
           end
           self.linearTexture:SetValue(0, progress)
         else

commit 137e56ff775e6132ac997dd3b47016044a40548a

Fix Automatic Progress Sources on sub elements
This is a regression introduced in  86bc2d91bbd5a5

UpdateProgressFromState is also used to copy the progress information
from the **region** to the sub element. The region acts as a pseudo
state for this mode.

Fixes: #5752

WeakAuras/RegionTypes/RegionPrototype.lua
@@ -408,7 +408,7 @@ local function UpdateProgressFromState(self, minMaxConfig, state, progressSource
   local remainingProperty = progressSource[8]
   local useAdditionalProgress = progressSource[9]
 
-  if not state or not state.show then
+  if not state then
     self.minProgress, self.maxProgress = nil, nil
     self.progressType = "timed"
     self.duration = 0