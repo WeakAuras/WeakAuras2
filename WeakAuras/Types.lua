474c2c8ca149201d39ce7d7b65545fbf65420c6a
Infus <infus@squorn.de>
2025-09-21 10:57:59 +0200

    Revert "Revert "Fix KR/TW/CN large number formatting for >= 100.000.000""
    This reverts commit 01bdf9c2be2005f8a22652d4f0e09b5c3a0b5fe8.
    
    Actually, this is still broken on Mists of Pandaria. The data is in the
    game files, but Mists is still using its own UIParent.lua, which
    contains the broken function instead of the shared UIParent.lua which
    contins the fixed function.
    
    Fixes: #6038

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -185,6 +185,43 @@ timeFormatter.GetMaxInterval = function(self)
   return #timeFormatIntervalDescriptionFixed
 end
 
+local AbbreviateNumbers = AbbreviateNumbers
+local gameLocale = GetLocale()
+if gameLocale == "koKR" or gameLocale == "zhCN" or gameLocale == "zhTW" then
+  -- Work around https://github.com/Stanzilla/WoWUIBugs/issues/515
+  --
+  local NUMBER_ABBREVIATION_DATA_FIXED={
+    [1]={
+      breakpoint = 10000 * 10000,
+      significandDivisor = 10000 * 10000,
+      abbreviation = SECOND_NUMBER_CAP_NO_SPACE,
+      fractionDivisor = 1
+    },
+    [2]={
+      breakpoint = 1000 * 10000,
+      significandDivisor = 1000 * 10000,
+      abbreviation = SECOND_NUMBER_CAP_NO_SPACE,
+      fractionDivisor = 10
+    },
+    [3]={
+      breakpoint = 10000,
+      significandDivisor = 1000,
+      abbreviation = FIRST_NUMBER_CAP_NO_SPACE,
+      fractionDivisor = 10
+    }
+  }
+
+  AbbreviateNumbers = function(value)
+    for i, data in ipairs(NUMBER_ABBREVIATION_DATA_FIXED) do
+      if value >= data.breakpoint then
+              local finalValue = math.floor(value / data.significandDivisor) / data.fractionDivisor;
+              return finalValue .. data.abbreviation;
+      end
+    end
+    return tostring(value);
+  end
+end
+
 local simpleFormatters = {
   AbbreviateNumbers = function(value)
     if type(value) == "string" then value = tonumber(value) end
