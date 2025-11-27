be60a77af35aa6ed625dcf9e267365465a2911bb
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2025-11-27 18:00:54 +0100

    Titan: fix repair dialog text assignment
    Based on PR #6107 by @barneyng

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -2441,10 +2441,11 @@ StaticPopupDialogs["WEAKAURAS_CONFIRM_REPAIR"] = {
     local AutomaticRepairText = L["WeakAuras has detected that it has been downgraded.\nYour saved auras may no longer work properly.\nWould you like to run the |cffff0000EXPERIMENTAL|r repair tool? This will overwrite any changes you have made since the last database upgrade.\nLast upgrade: %s\n\n|cffff0000You should BACKUP your WTF folder BEFORE pressing this button.|r"]
     local ManualRepairText = L["Are you sure you want to run the |cffff0000EXPERIMENTAL|r repair tool?\nThis will overwrite any changes you have made since the last database upgrade.\nLast upgrade: %s"]
 
+    local text = WeakAuras.IsWrathClassic() and self.Text or self.text
     if self.data.reason == "user" then
-      self.text:SetText(ManualRepairText:format(LastUpgrade()))
+      text:SetText(ManualRepairText:format(LastUpgrade()))
     else
-      self.text:SetText(AutomaticRepairText:format(LastUpgrade()))
+      text:SetText(AutomaticRepairText:format(LastUpgrade()))
     end
   end,
   OnCancel = function(self)
