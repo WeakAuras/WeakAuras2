e2f7f4f7d1ae1c0e91c1e74bf16c918084606a83
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2025-12-20 02:18:35 +0100

    Titan: remove unused talent_types_specific

--- WeakAuras/WeakAuras.lua
+++ WeakAuras/WeakAuras.lua
@@ -983,7 +983,7 @@ local function CreateTalentCache()
 
   Private.talent_types_specific[player_class] = Private.talent_types_specific[player_class] or {};
 
-  if WeakAuras.IsClassicOrWrathOrCata() then
+  if WeakAuras.IsClassicOrCata() then
     for tab = 1, GetNumTalentTabs() do
       for num_talent = 1, GetNumTalents(tab) do
         local talentName, talentIcon = Private.ExecEnv.GetTalentInfo(tab, num_talent);
@@ -993,7 +993,7 @@ local function CreateTalentCache()
         end
       end
     end
-  elseif WeakAuras.IsMists() then
+  elseif WeakAuras.IsWrathOrMists() then
     -- unused
   else
     local spec = GetSpecialization()
