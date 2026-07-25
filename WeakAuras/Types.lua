915e98cce9ffcef33f03ac42bfcefdce83f44ef2
Infus <infus@squorn.de>
2026-07-25 23:16:13 +0200

    Remove AbbreviateNumbers workaround since it's no longer needed
    This time for sure!
    
    Fingers crossed.

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -182,61 +182,6 @@ timeFormatter.GetMaxInterval = function(self)
   return #timeFormatIntervalDescriptionFixed
 end
 
-local AbbreviateNumbers = AbbreviateNumbers
-local gameLocale = GetLocale()
-if gameLocale == "koKR" or gameLocale == "zhCN" or gameLocale == "zhTW" then
-  -- Work around https://github.com/Stanzilla/WoWUIBugs/issues/515
-  --
-  local NUMBER_ABBREVIATION_DATA_FIXED={
-    [1]={
-      breakpoint = 10000 * 10000,
-      significandDivisor = 10000 * 10000,
-      abbreviation = SECOND_NUMBER_CAP_NO_SPACE,
-      fractionDivisor = 1
-    },
-    [2]={
-      breakpoint = 1000 * 10000,
-      significandDivisor = 1000 * 10000,
-      abbreviation = SECOND_NUMBER_CAP_NO_SPACE,
-      fractionDivisor = 10
-    },
-    [3]={
-      breakpoint = 10000,
-      significandDivisor = 1000,
-      abbreviation = FIRST_NUMBER_CAP_NO_SPACE,
-      fractionDivisor = 10
-    }
-  }
-
-  AbbreviateNumbers = function(value)
-    for i, data in ipairs(NUMBER_ABBREVIATION_DATA_FIXED) do
-      if value >= data.breakpoint then
-              local finalValue = math.floor(value / data.significandDivisor) / data.fractionDivisor;
-              return finalValue .. data.abbreviation;
-      end
-    end
-    return tostring(value);
-  end
-elseif WeakAuras.IsClassicOrTBCOrWrathOrCataOrMists() then
-  local NUMBER_ABBREVIATION_DATA_FIXED = {
-        -- Work around another bug in NUMBER_ABBREVIATION_DATA, https://github.com/WeakAuras/WeakAuras2/issues/6061
-        { breakpoint = 10000000,        abbreviation = SECOND_NUMBER_CAP_NO_SPACE,      significandDivisor = 1000000,   fractionDivisor = 1 },
-        { breakpoint = 1000000,         abbreviation = SECOND_NUMBER_CAP_NO_SPACE,      significandDivisor = 100000,            fractionDivisor = 10 },
-        { breakpoint = 10000,           abbreviation = FIRST_NUMBER_CAP_NO_SPACE,       significandDivisor = 1000,              fractionDivisor = 1 },
-        { breakpoint = 1000,            abbreviation = FIRST_NUMBER_CAP_NO_SPACE,       significandDivisor = 100,               fractionDivisor = 10 }
-  }
-
-  AbbreviateNumbers = function(value)
-    for i, data in ipairs(NUMBER_ABBREVIATION_DATA_FIXED) do
-      if value >= data.breakpoint then
-              local finalValue = math.floor(value / data.significandDivisor) / data.fractionDivisor;
-              return finalValue .. data.abbreviation;
-      end
-    end
-    return tostring(value);
-  end
-end
-
 local simpleFormatters = {
   AbbreviateNumbers = function(value)
     if type(value) == "string" then value = tonumber(value) end
