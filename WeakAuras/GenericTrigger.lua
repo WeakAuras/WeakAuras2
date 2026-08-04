WeakAuras/WeakAuras2

commit a8d977a9b0d3f737f5a8695d8d3b3a97d112cb30

Upgrade API and Fixes for Vanilla

WeakAuras/GenericTrigger.lua
@@ -2304,12 +2304,19 @@ do
 
   local function CheckGCD()
     local event;
-    local startTime, duration, _, modRate
+    local startTime, duration, modRate
     if WeakAuras.IsClassicOrTBCOrWrath() then
-      startTime, duration = GetSpellCooldown(29515);
-      shootStart, shootDuration = GetSpellCooldown(5019)
-    elseif GetSpellCooldown then
-      startTime, duration, _, modRate = GetSpellCooldown(61304);
+      local spellCooldownInfo = C_Spell.GetSpellCooldown(29515)
+      if spellCooldownInfo then
+        startTime = spellCooldownInfo.startTime
+        duration = spellCooldownInfo.duration
+        modRate = spellCooldownInfo.modRate
+      end
+      local shootInfo = C_Spell.GetSpellCooldown(5019)
+      if shootInfo then
+        shootStart = shootInfo.startTime
+        shootDuration = shootInfo.duration
+      end
     else
       local spellCooldownInfo = C_Spell.GetSpellCooldown(61304);
       if spellCooldownInfo then
@@ -3277,19 +3284,12 @@ do
   ---@param runeDuration? number
   function WeakAuras.GetSpellCooldownUnified(id, runeDuration)
     local startTimeCooldown, durationCooldown, enabled, modRate
-    if GetSpellCooldown then
-      startTimeCooldown, durationCooldown, enabled, modRate = GetSpellCooldown(id)
-      if type(enabled) == "number" then
-        enabled = enabled == 1 and true or false
-      end
-    else
-      local spellCooldownInfo = C_Spell.GetSpellCooldown(id);
-      if spellCooldownInfo then
-        startTimeCooldown = spellCooldownInfo.startTime
-        durationCooldown = spellCooldownInfo.duration
-        enabled = spellCooldownInfo.isEnabled
-        modRate = spellCooldownInfo.modRate
-      end
+    local spellCooldownInfo = C_Spell.GetSpellCooldown(id);
+    if spellCooldownInfo then
+      startTimeCooldown = spellCooldownInfo.startTime
+      durationCooldown = spellCooldownInfo.duration
+      enabled = spellCooldownInfo.isEnabled
+      modRate = spellCooldownInfo.modRate
     end
 
     local charges, maxCharges, startTimeCharges, durationCharges, modRateCharges = GetSpellCharges(id);
@@ -4127,7 +4127,7 @@ if WeakAuras.IsClassicOrTBCOrWrath() then
           for _, spellID in ipairs(queueableSpells) do
             -- Check the highest known rank
             local maxRank = select(7, Private.ExecEnv.GetSpellInfo(Private.ExecEnv.GetSpellName(spellID)))
-            if IsCurrentSpell(maxRank) then
+            if C_Spell.IsCurrentSpell(maxRank) then
               newQueuedSpell = maxRank
               break
             end
@@ -4147,7 +4147,7 @@ if WeakAuras.IsClassicOrTBCOrWrath() then
   end
 end
 
-local GetSpellPowerCost = GetSpellPowerCost or C_Spell and C_Spell.GetSpellPowerCost
+local GetSpellPowerCost = C_Spell.GetSpellPowerCost
 
 ---@param powerTypeToCheck integer
 ---@return number? cost

WeakAuras/BaseRegions/StopMotion.lua
@@ -168,11 +168,9 @@ function Private.StopMotionBase.setTextureFunc(textureWidget, texturePath, textu
   end
 end
 
-local GetAtlasInfo = WeakAuras.IsClassicEra() and GetAtlasInfo or C_Texture.GetAtlasInfo
-
 --- @type fun(self: StopMotionBaseInstance, texture: string)
 local function SetTextureViaAtlas(self, texture)
-  if type(texture) == "string" and GetAtlasInfo(texture) then
+  if type(texture) == "string" and C_Texture.GetAtlasInfo(texture) then
     self.texture:SetAtlas(texture)
   else
     self.texture:SetTexture(texture)

WeakAuras/Prototypes.lua
@@ -1906,7 +1906,7 @@ Private.load_prototype = {
     },
     {
       name = "role",
-      display = (WeakAuras.IsClassic()) and L["Assigned Role"] or L["Spec Role"],
+      display = (WeakAuras.IsClassicEra()) and L["Assigned Role"] or L["Spec Role"],
       type = "multiselect",
       values = "role_types",
       init = "arg",
@@ -11277,7 +11277,7 @@ Private.event_prototypes = {
         name = "vehicle",
         display = WeakAuras.IsClassicOrTBC() and L["On Taxi"] or L["In Vehicle"],
         type = "tristate",
-        init = WeakAuras.IsClassicEra() and "UnitOnTaxi('player')" or "UnitInVehicle('player') or UnitOnTaxi('player')",
+        init = "UnitInVehicle('player') or UnitOnTaxi('player')",
       },
       {
         name = "resting",

WeakAuras/Types.lua
@@ -126,9 +126,6 @@ Private.big_number_types = {
   ["AbbreviateLargeNumbers"] = L["AbbreviateLargeNumbers (Blizzard)"],
   ["BreakUpLargeNumbers"] = L["BreakUpLargeNumbers (Blizzard)"],
 }
-if WeakAuras.IsClassicEra() then
-  Private.big_number_types.BreakUpLargeNumbers = nil
-end
 ---@type table<string, string>
 Private.big_number_types_with_disable = CopyTable(Private.big_number_types)
 Private.big_number_types_with_disable["disable"] = L["Disabled"]
@@ -220,7 +217,7 @@ if gameLocale == "koKR" or gameLocale == "zhCN" or gameLocale == "zhTW" then
     end
     return tostring(value);
   end
-elseif WeakAuras.IsClassicEra() or WeakAuras.IsWrathOrCataOrMists() then
+elseif WeakAuras.IsClassicOrTBCOrWrathOrCataOrMists() then
   local NUMBER_ABBREVIATION_DATA_FIXED = {
         -- Work around another bug in NUMBER_ABBREVIATION_DATA, https://github.com/WeakAuras/WeakAuras2/issues/6061
         { breakpoint = 10000000,        abbreviation = SECOND_NUMBER_CAP_NO_SPACE,      significandDivisor = 1000000,   fractionDivisor = 1 },

WeakAuras/WeakAuras.lua
@@ -1719,13 +1719,10 @@ local function scanForLoadsImpl(toCheck, event, arg1, ...)
       end
     end
   end
-  if WeakAuras.IsClassicEra() then
-    vehicle = UnitOnTaxi('player')
-  end
-  if WeakAuras.IsTBCOrWrathOrCataOrMistsOrRetail() then
-    vehicle = UnitInVehicle('player') or UnitOnTaxi('player') or false
-    vehicleUi = UnitHasVehicleUI('player') or HasOverrideActionBar() or HasVehicleActionBar() or false
-  end
+
+  vehicle = UnitInVehicle('player') or UnitOnTaxi('player') or false
+  vehicleUi = UnitHasVehicleUI('player') or HasOverrideActionBar() or HasVehicleActionBar() or false
+
   if WeakAuras.IsCataOrMistsOrRetail() then
     specId, role, position = Private.LibSpecWrapper.SpecRolePositionForUnit("player")
   end

WeakAurasOptions/Cache.lua
@@ -40,6 +40,7 @@ function spellCache.Build()
     holes[285224] = 301088
     holes[301101] = 324269
     holes[474742] = 1213143
+    holes[1259266] = 1278761
   elseif WeakAuras.IsTBC() then
     holes = {}
     holes[81748] = 158262

WeakAurasOptions/LoadOptions.lua
@@ -23,7 +23,7 @@ local function CorrectSpellName(input)
   local inputId = tonumber(input)
   if(inputId) then
     return inputId
-  elseif WeakAuras.IsClassicEra() and input then
+  elseif WeakAuras.IsClassicEra() and input then -- Classic does not create a hyperlink, instead returns plain spellName
     local _, _, _, _, _, _, spellId = OptionsPrivate.Private.ExecEnv.GetSpellInfo(input)
     if spellId then
       return spellId

WeakAurasOptions/RegionOptions/ProgressTexture.lua
@@ -5,7 +5,6 @@ local AddonName = ...
 local OptionsPrivate = select(2, ...)
 
 local L = WeakAuras.L;
-local GetAtlasInfo = WeakAuras.IsClassicEra() and GetAtlasInfo or C_Texture.GetAtlasInfo
 
 local function createOptions(id, data)
   local options = {
@@ -449,7 +448,7 @@ local function createThumbnail()
   local OrgSetTexture = foreground.SetTexture;
   -- WORKAROUND, setting the same texture with a different wrap mode does not change the wrap mode
   foreground.SetTexture = function(self, texture, horWrapMode, verWrapMode)
-    if (GetAtlasInfo(texture)) then
+    if (C_Texture.GetAtlasInfo(texture)) then
       self:SetAtlas(texture);
     else
       local needToClear = (self.horWrapMode and self.horWrapMode ~= horWrapMode) or (self.verWrapMode and self.verWrapMode ~= verWrapMode);

commit 45355353a87f73b3f4933936081e2685ea0a8703

Fix regression in queued spell detection
This fixes a regression introduced in a8d977a9, where
C_Spell.IsCurrentSpell could be called with a nil spell ID when no
known spell rank was found.

Unknown queueable spells are now skipped.

WeakAuras/GenericTrigger.lua
@@ -4127,7 +4127,7 @@ if WeakAuras.IsClassicOrTBCOrWrath() then
           for _, spellID in ipairs(queueableSpells) do
             -- Check the highest known rank
             local maxRank = select(7, Private.ExecEnv.GetSpellInfo(Private.ExecEnv.GetSpellName(spellID)))
-            if C_Spell.IsCurrentSpell(maxRank) then
+            if maxRank and C_Spell.IsCurrentSpell(maxRank) then
               newQueuedSpell = maxRank
               break
             end